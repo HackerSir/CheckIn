5e048725bdd0e4fd8352b0217628d491a85c3bc8
Chan, Danny <danny50610@yahoo.com.tw>
2017-07-10 20:11:27 +0800

    Change view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,86 +1,90 @@
 @extends('layouts.app')
 
-@section('title', '註冊')
+@section('title', '管理員註冊')
 
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            註冊
-        </div>
-        <div class="card-block">
-            <form role="form" method="POST" action="{{ route('register') }}">
-                {{ csrf_field() }}
+    <div class="row mt-3">
+        <div class="col-md-8 offset-md-2">
+            <h1>管理員註冊</h1>
+            <div class="card">
+                <div class="card-block">
+                    <form role="form" method="POST" action="{{ route('register') }}">
+                        {{ csrf_field() }}
 
-                <div class="form-group row{{ $errors->has('name') ? ' has-danger' : '' }}">
-                    <label for="name" class="col-md-4 form-control-label">名稱</label>
+                        <div class="form-group row{{ $errors->has('name') ? ' has-danger' : '' }}">
+                            <label for="name" class="col-md-2 form-control-label">名稱</label>
 
-                    <div class="col-md-6">
-                        <input id="name" type="text"
-                               class="form-control{{ $errors->has('name') ? ' form-control-danger' : '' }}" name="name"
-                               value="{{ old('name') }}" required
-                               autofocus>
+                            <div class="col-md-10">
+                                <input id="name" type="text"
+                                       class="form-control{{ $errors->has('name') ? ' form-control-danger' : '' }}"
+                                       name="name"
+                                       value="{{ old('name') }}" required
+                                       autofocus>
 
-                        @if ($errors->has('name'))
-                            <span class="form-control-feedback">
+                                @if ($errors->has('name'))
+                                    <span class="form-control-feedback">
                                 <strong>{{ $errors->first('name') }}</strong>
                             </span>
-                        @endif
-                    </div>
-                </div>
+                                @endif
+                            </div>
+                        </div>
 
-                <div class="form-group row{{ $errors->has('email') ? ' has-danger' : '' }}">
-                    <label for="email" class="col-md-4 form-control-label">信箱</label>
+                        <div class="form-group row{{ $errors->has('email') ? ' has-danger' : '' }}">
+                            <label for="email" class="col-md-2 form-control-label">信箱</label>
 
-                    <div class="col-md-6">
-                        <input id="email" type="email"
-                               class="form-control{{ $errors->has('email') ? ' form-control-danger' : '' }}"
-                               name="email" value="{{ old('email') }}"
-                               required>
+                            <div class="col-md-10">
+                                <input id="email" type="email"
+                                       class="form-control{{ $errors->has('email') ? ' form-control-danger' : '' }}"
+                                       name="email" value="{{ old('email') }}"
+                                       required>
 
-                        @if ($errors->has('email'))
-                            <span class="form-control-feedback">
+                                @if ($errors->has('email'))
+                                    <span class="form-control-feedback">
                                 <strong>{{ $errors->first('email') }}</strong>
                             </span>
-                        @endif
-                    </div>
-                </div>
+                                @endif
+                            </div>
+                        </div>
 
-                <div class="form-group row{{ $errors->has('password') ? ' has-danger' : '' }}">
-                    <label for="password" class="col-md-4 form-control-label">密碼</label>
+                        <div class="form-group row{{ $errors->has('password') ? ' has-danger' : '' }}">
+                            <label for="password" class="col-md-2 form-control-label">密碼</label>
 
-                    <div class="col-md-6">
-                        <input id="password" type="password"
-                               class="form-control{{ $errors->has('password') ? ' form-control-danger' : '' }}"
-                               name="password" required>
+                            <div class="col-md-10">
+                                <input id="password" type="password"
+                                       class="form-control{{ $errors->has('password') ? ' form-control-danger' : '' }}"
+                                       name="password" required>
 
-                        @if ($errors->has('password'))
-                            <span class="form-control-feedback">
+                                @if ($errors->has('password'))
+                                    <span class="form-control-feedback">
                                 <strong>{{ $errors->first('password') }}</strong>
                             </span>
-                        @endif
-                    </div>
-                </div>
+                                @endif
+                            </div>
+                        </div>
 
-                <div class="form-group row">
-                    <label for="password-confirm" class="col-md-4 form-control-label">確認密碼</label>
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-2 form-control-label">確認密碼</label>
 
-                    <div class="col-md-6">
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
-                               required>
-                    </div>
-                </div>
+                            <div class="col-md-10">
+                                <input id="password-confirm" type="password" class="form-control"
+                                       name="password_confirmation"
+                                       required>
+                            </div>
+                        </div>
 
-                <div class="form-group row">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-primary">
-                            註冊
-                        </button>
-                        <a class="btn btn-link" href="{{ route('login') }}">
-                            登入
-                        </a>
-                    </div>
+                        <div class="form-group row">
+                            <div class="col-md-10 offset-md-2">
+                                <button type="submit" class="btn btn-primary">
+                                    註冊
+                                </button>
+                                <a class="btn btn-link" href="{{ route('login') }}">
+                                    登入
+                                </a>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/auth/resend-confirm-mail.blade.php
+++ resources/views/auth/resend-confirm-mail.blade.php
@@ -3,38 +3,40 @@
 @section('title', '重新發送驗證信件')
 
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            重新發送驗證信件
-        </div>
-        <div class="card-block">
-            <form role="form" method="POST" action="{{ route('confirm-mail.resend') }}">
-                {{ csrf_field() }}
+    <div class="row mt-3">
+        <div class="col-md-8 offset-md-2">
+            <h1>重新發送驗證信件</h1>
+            <div class="card">
+                <div class="card-block">
+                    <form role="form" method="POST" action="{{ route('confirm-mail.resend') }}">
+                        {{ csrf_field() }}
 
-                <div class="form-group row{{ $errors->has('email') ? ' has-danger' : '' }}">
-                    <label for="email" class="col-md-4 form-control-label">信箱</label>
+                        <div class="form-group row{{ $errors->has('email') ? ' has-danger' : '' }}">
+                            <label for="email" class="col-md-2 form-control-label">信箱</label>
 
-                    <div class="col-md-6">
-                        <input id="email" type="email" class="form-control" value="{{ $user->email }}" readonly>
-                        <span class="form-text">信箱作為帳號使用，故無法修改</span>
-                    </div>
-                </div>
-                <div class="alert alert-warning" role="alert">
-                    請注意：
-                    <ul>
-                        <li>若信箱錯誤，請重新註冊帳號</li>
-                        <li>發送前，請先確認信箱是否屬於自己</li>
-                        <li>發送信件可能耗費幾分鐘，請耐心等待</li>
-                        <li>僅最後一次發送之信件有效</li>
-                    </ul>
-                </div>
+                            <div class="col-md-10">
+                                <input id="email" type="email" class="form-control" value="{{ $user->email }}" readonly>
+                                <span class="form-text">信箱作為帳號使用，故無法修改</span>
+                            </div>
+                        </div>
+                        <div class="alert alert-warning col-md-10 offset-md-2" role="alert">
+                            請注意：
+                            <ul>
+                                <li>若信箱錯誤，請重新註冊帳號</li>
+                                <li>發送前，請先確認信箱是否屬於自己</li>
+                                <li>發送信件可能耗費幾分鐘，請耐心等待</li>
+                                <li>僅最後一次發送之信件有效</li>
+                            </ul>
+                        </div>
 
-                <div class="form-group row">
-                    <div class="col-md-8 offset-md-4">
-                        <button type="submit" class="btn btn-primary">發送驗證信件</button>
-                    </div>
+                        <div class="form-group row">
+                            <div class="col-md-10 offset-md-2">
+                                <button type="submit" class="btn btn-primary">發送驗證信件</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     </div>
 @endsection
