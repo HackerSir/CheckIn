80fa53218604c9de9f5f90c02152facd6f687f0d
Chan, Danny <danny50610@yahoo.com.tw>
2017-07-10 20:29:40 +0800

    Change view

--- resources/views/auth/passwords/change-password.blade.php
+++ resources/views/auth/passwords/change-password.blade.php
@@ -3,70 +3,72 @@
 @section('title', '修改密碼')
 
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            修改密碼
-        </div>
-        <div class="card-block">
-            <form role="form" method="POST" action="{{ route('password.change') }}">
-                {{ csrf_field() }}
-                {{ method_field('put') }}
+    <div class="row mt-3">
+        <div class="col-md-8 offset-md-2">
+            <h1>修改密碼</h1>
+            <div class="card">
+                <div class="card-block">
+                    <form role="form" method="POST" action="{{ route('password.change') }}">
+                        {{ csrf_field() }}
+                        {{ method_field('put') }}
 
-                <div class="form-group row{{ $errors->has('password') ? ' has-danger' : '' }}">
-                    <label for="password" class="col-md-4 form-control-label">密碼</label>
+                        <div class="form-group row{{ $errors->has('password') ? ' has-danger' : '' }}">
+                            <label for="password" class="col-md-2 col-form-label">密碼</label>
 
-                    <div class="col-md-6">
-                        <input id="password" type="password"
-                               class="form-control{{ $errors->has('password') ? ' form-control-danger' : '' }}"
-                               name="password" required autofocus>
+                            <div class="col-md-10">
+                                <input id="password" type="password"
+                                       class="form-control{{ $errors->has('password') ? ' form-control-danger' : '' }}"
+                                       name="password" required autofocus>
 
-                        @if ($errors->has('password'))
-                            <span class="form-control-feedback">
+                                @if ($errors->has('password'))
+                                    <span class="form-control-feedback">
                                 <strong>{{ $errors->first('password') }}</strong>
                             </span>
-                        @endif
-                    </div>
-                </div>
+                                @endif
+                            </div>
+                        </div>
 
-                <div class="form-group row{{ $errors->has('new_password') ? ' has-danger' : '' }}">
-                    <label for="new_password" class="col-md-4 form-control-label">新密碼</label>
+                        <div class="form-group row{{ $errors->has('new_password') ? ' has-danger' : '' }}">
+                            <label for="new_password" class="col-md-2 col-form-label">新密碼</label>
 
-                    <div class="col-md-6">
-                        <input id="new_password" type="password"
-                               class="form-control{{ $errors->has('new_password') ? ' form-control-danger' : '' }}"
-                               name="new_password" required autofocus>
+                            <div class="col-md-10">
+                                <input id="new_password" type="password"
+                                       class="form-control{{ $errors->has('new_password') ? ' form-control-danger' : '' }}"
+                                       name="new_password" required autofocus>
 
-                        @if ($errors->has('new_password'))
-                            <span class="form-control-feedback">
+                                @if ($errors->has('new_password'))
+                                    <span class="form-control-feedback">
                                 <strong>{{ $errors->first('new_password') }}</strong>
                             </span>
-                        @endif
-                    </div>
-                </div>
+                                @endif
+                            </div>
+                        </div>
 
-                <div class="form-group row{{ $errors->has('new_password_confirmation') ? ' has-danger' : '' }}">
-                    <label for="new_password_confirmation" class="col-md-4 form-control-label">確認新密碼</label>
+                        <div class="form-group row{{ $errors->has('new_password_confirmation') ? ' has-danger' : '' }}">
+                            <label for="new_password_confirmation" class="col-md-2 col-form-label">確認新密碼</label>
 
-                    <div class="col-md-6">
-                        <input id="new_password_confirmation" type="password"
-                               class="form-control{{ $errors->has('new_password_confirmation') ? ' form-control-danger' : '' }}"
-                               name="new_password_confirmation" required autofocus>
+                            <div class="col-md-10">
+                                <input id="new_password_confirmation" type="password"
+                                       class="form-control{{ $errors->has('new_password_confirmation') ? ' form-control-danger' : '' }}"
+                                       name="new_password_confirmation" required autofocus>
 
-                        @if ($errors->has('new_password_confirmation'))
-                            <span class="form-control-feedback">
+                                @if ($errors->has('new_password_confirmation'))
+                                    <span class="form-control-feedback">
                                 <strong>{{ $errors->first('new_password_confirmation') }}</strong>
                             </span>
-                        @endif
-                    </div>
-                </div>
+                                @endif
+                            </div>
+                        </div>
 
-                <div class="form-group row">
-                    <div class="col-md-8 offset-md-4">
-                        <a href="{{ route('profile') }}" class="btn btn-secondary">返回個人資料</a>
-                        <button type="submit" class="btn btn-primary"> 更新密碼</button>
-                    </div>
+                        <div class="form-group row">
+                            <div class="col-md-10 offset-md-2">
+                                <button type="submit" class="btn btn-primary">修改密碼</button>
+                                <a href="{{ route('profile') }}" class="btn btn-secondary">返回個人資料</a>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     </div>
 @endsection
