3bfd0fb3e25b743ab8e7a81a9e50a98970a03e38
Chan, Danny <danny50610@yahoo.com.tw>
2017-07-10 20:20:32 +0800

    Change view

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,44 +3,46 @@
 @section('title', '重設密碼')
 
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            重設密碼
-        </div>
-        <div class="card-block">
-            @if (session('status'))
-                <div class="alert alert-success">
-                    {{ session('status') }}
-                </div>
-            @endif
-            <form role="form" method="POST" action="{{ route('password.email') }}">
-                {{ csrf_field() }}
+    <div class="row mt-3">
+        <div class="col-md-8 offset-md-2">
+            <h1>重設密碼</h1>
+            <div class="card">
+                <div class="card-block">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <form role="form" method="POST" action="{{ route('password.email') }}">
+                        {{ csrf_field() }}
 
-                <div class="form-group row{{ $errors->has('email') ? ' has-danger' : '' }}">
-                    <label for="email" class="col-md-4 form-control-label">信箱</label>
+                        <div class="form-group row{{ $errors->has('email') ? ' has-danger' : '' }}">
+                            <label for="email" class="col-md-2 form-control-label">信箱</label>
 
-                    <div class="col-md-6">
-                        <input id="email" type="email"
-                               class="form-control{{ $errors->has('email') ? ' form-control-danger' : '' }}"
-                               name="email" value="{{ old('email') }}"
-                               required>
+                            <div class="col-md-10">
+                                <input id="email" type="email"
+                                       class="form-control{{ $errors->has('email') ? ' form-control-danger' : '' }}"
+                                       name="email" value="{{ old('email') }}"
+                                       required>
 
-                        @if ($errors->has('email'))
-                            <span class="form-control-feedback">
+                                @if ($errors->has('email'))
+                                    <span class="form-control-feedback">
                                 <strong>{{ $errors->first('email') }}</strong>
                             </span>
-                        @endif
-                    </div>
-                </div>
+                                @endif
+                            </div>
+                        </div>
 
-                <div class="form-group row">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-primary">
-                            發送密碼重設連結
-                        </button>
-                    </div>
+                        <div class="form-group row">
+                            <div class="col-md-10 offset-md-2">
+                                <button type="submit" class="btn btn-primary">
+                                    發送密碼重設連結
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     </div>
 @endsection
