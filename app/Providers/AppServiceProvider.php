7cae1b3ca3f2e8b7c668acabf86bdfdf8605f287
KID <jyhsu2000@yahoo.com.tw>
2017-07-10 20:22:17 +0800

    Add settings of slack notification

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
 use Schema;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,23 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        //Slack通知
+        $slackEnable = env('SLACK_ENABLE', false) === true;
+        $slackToken = env('SLACK_TOKEN');
+        $slackChannel = env('SLACK_CHANNEL');
+        if ($slackEnable && $slackToken && $slackChannel) {
+            $monolog = \Log::getMonolog();
+            $slackHandler = new \Monolog\Handler\SlackHandler(
+                $slackToken,
+                $slackChannel,
+                'Monolog',
+                true,
+                null,
+                Logger::WARNING
+            );
+            $monolog->pushHandler($slackHandler);
+        }
     }
 
     /**
