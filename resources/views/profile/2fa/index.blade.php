f5177158efcb32311356566aaa3181b6ba84ad3b
Chan, Danny <danny50610@yahoo.com.tw>
2017-07-12 00:09:48 +0800

    Fix merge bug

--- resources/views/profile/2fa/index.blade.php
+++ resources/views/profile/2fa/index.blade.php
@@ -11,62 +11,64 @@
                     <form role="form" method="POST" action="{{ route('profile.2fa.toggle') }}">
                         {{ csrf_field() }}
 
-                <div class="form-group row">
-                    <label class="col-md-4 col-form-label">目前狀態</label>
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label">目前狀態</label>
 
-                    <div class="col-md-6">
-                        @if($user->google2fa_secret)
-                            <span class="form-text text-success">已啟用</span>
-                        @else
-                            <span class="form-text text-danger">未啟用</span>
-                        @endif
-                    </div>
-                </div>
-                @if(isset($google2faQRCodeUrl))
-                    <div class="form-group row">
-                        <label class="col-md-4 col-form-label">QR Code</label>
-
-                        <div class="col-md-6">
-                            <img src="{{ $google2faQRCodeUrl }}"><br/>
-                            請以 <a
-                                href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2"
-                                target="_blank">Google Authenticator</a> 掃描此二維條碼，並於下方輸入驗證碼
+                            <div class="col-md-6">
+                                @if($user->google2fa_secret)
+                                    <span class="form-text text-success">已啟用</span>
+                                @else
+                                    <span class="form-text text-danger">未啟用</span>
+                                @endif
+                            </div>
                         </div>
-                    </div>
-                @endif
+                        @if(isset($google2faQRCodeUrl))
+                            <div class="form-group row">
+                                <label class="col-md-4 col-form-label">QR Code</label>
 
-                <div class="form-group row{{ $errors->has('one_time_password') ? ' has-danger' : '' }}">
-                    <label for="one_time_password" class="col-md-4 col-form-label">驗證碼</label>
+                                <div class="col-md-6">
+                                    <img src="{{ $google2faQRCodeUrl }}"><br/>
+                                    請以 <a
+                                        href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2"
+                                        target="_blank">Google Authenticator</a> 掃描此二維條碼，並於下方輸入驗證碼
+                                </div>
+                            </div>
+                        @endif
 
-                    <div class="col-md-6">
-                        <input id="one_time_password" type="text"
-                               class="form-control{{ $errors->has('one_time_password') ? ' form-control-danger' : '' }}"
-                               name="one_time_password" required autofocus>
-                        <span class="form-text">請輸入 <a
-                            href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2"
-                            target="_blank">Google Authenticator</a> 顯示之驗證碼</span>
+                        <div class="form-group row{{ $errors->has('one_time_password') ? ' has-danger' : '' }}">
+                            <label for="one_time_password" class="col-md-4 col-form-label">驗證碼</label>
 
-                        @if ($errors->has('one_time_password'))
-                            <span class="form-control-feedback">
+                            <div class="col-md-6">
+                                <input id="one_time_password" type="text"
+                                       class="form-control{{ $errors->has('one_time_password') ? ' form-control-danger' : '' }}"
+                                       name="one_time_password" required autofocus>
+                                <span class="form-text">請輸入 <a
+                                        href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2"
+                                        target="_blank">Google Authenticator</a> 顯示之驗證碼</span>
+
+                                @if ($errors->has('one_time_password'))
+                                    <span class="form-control-feedback">
                                 <strong>{{ $errors->first('one_time_password') }}</strong>
                             </span>
-                        @endif
-                    </div>
-                </div>
+                                @endif
+                            </div>
+                        </div>
 
-                <div class="form-group row">
-                    <div class="col-md-8 offset-md-4">
-                        @if($user->google2fa_secret)
-                            <button type="submit" class="btn btn-danger">停用</button>
-                        @else
-                            {{ Form::hidden('toggle', 'on') }}
-                            <button type="submit" class="btn btn-primary">啟用</button>
-                        @endif
+                        <div class="form-group row">
+                            <div class="col-md-8 offset-md-4">
+                                @if($user->google2fa_secret)
+                                    <button type="submit" class="btn btn-danger">停用</button>
+                                @else
+                                    {{ Form::hidden('toggle', 'on') }}
+                                    <button type="submit" class="btn btn-primary">啟用</button>
+                                @endif
 
-                        <a href="{{ route('profile') }}" class="btn btn-secondary">返回個人資料</a>
-                    </div>
+                                <a href="{{ route('profile') }}" class="btn btn-secondary">返回個人資料</a>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     </div>
 @endsection
