af84fe69059372c8a45fdaa3b5f4265ae389cbb1
Chan, Danny <danny50610@yahoo.com.tw>
2017-07-11 23:59:38 +0800

    Change profile view

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -11,71 +11,75 @@
 @endsection
 
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            個人資料
-        </div>
-        <div class="text-center">
-            {{-- Gravatar大頭貼 --}}
-            <a href="https://zh-tw.gravatar.com/" target="_blank" title="透過Gravatar更換照片">
-                <img src="{{ Gravatar::src($user->email, 200) }}" class="img-thumbnail" id="gravatar"/>
-            </a>
-        </div>
-        <div class="card-block">
-            <table class="table table-hover">
-                <tr>
-                    <td class="text-md-right">名稱：</td>
-                    <td>{{ $user->name }}</td>
-                </tr>
-                <tr>
-                    <td class="text-md-right">Email：</td>
-                    <td>{{ $user->email }}</td>
-                </tr>
-                @if($user->is_local_account)
-                    <tr>
-                        <td class="text-md-right">兩步驟驗證：</td>
-                        <td>
-                            <a href="{{ route('profile.2fa.index') }}">
+    <div class="row mt-3">
+        <div class="col-md-8 offset-md-2">
+            <h1>個人資料</h1>
+            <div class="card">
+                <div class="text-center">
+                    {{-- Gravatar大頭貼 --}}
+                    <a href="https://zh-tw.gravatar.com/" target="_blank" title="透過Gravatar更換照片">
+                        <img src="{{ Gravatar::src($user->email, 200) }}" class="img-thumbnail" id="gravatar"/>
+                    </a>
+                </div>
+                <div class="card-block">
+                    <table class="table table-hover">
+                        <tr>
+                            <td class="text-md-right">名稱：</td>
+                            <td>{{ $user->name }}</td>
+                        </tr>
+                        <tr>
+                            <td class="text-md-right">Email：</td>
+                            <td>{{ $user->email }}</td>
+                        </tr>
+                        @if($user->is_local_account)
+                        <tr>
+                            <td class="text-md-right">兩步驟驗證：</td>
+                            <td>
+
                                 @if($user->google2fa_secret)
                                     <span class="text-success">已啟用</span>
                                 @else
                                     <span class="text-danger">未啟用</span>
                                 @endif
-                            </a>
-                        </td>
-                    </tr>
+                                <a href="{{ route('profile.2fa.index') }}">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        @endif
+                        <tr>
+                            <td class="text-md-right">角色：</td>
+                            <td>
+                                @foreach($user->roles as $role)
+                                    {{ $role->display_name }}<br/>
+                                @endforeach
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="text-md-right">註冊時間：</td>
+                            <td>{{ $user->register_at }}</td>
+                        </tr>
+                        <tr>
+                            <td class="text-md-right">註冊IP：</td>
+                            <td>{{ $user->register_ip }}</td>
+                        </tr>
+                        <tr>
+                            <td class="text-md-right">最後登入時間：</td>
+                            <td>{{ $user->last_login_at }}</td>
+                        </tr>
+                        <tr>
+                            <td class="text-md-right">最後登入IP：</td>
+                            <td>{{ $user->last_login_ip }}</td>
+                        </tr>
+                    </table>
+                </div>
+                @if($user->is_local_account)
+                    <div class="card-block text-center">
+                        <a href="{{ route('profile.edit') }}" class="btn btn-primary">編輯資料</a>
+                        <a href="{{ route('password.change') }}" class="btn btn-primary">修改密碼</a>
+                    </div>
                 @endif
-                <tr>
-                    <td class="text-md-right">角色：</td>
-                    <td>
-                        @foreach($user->roles as $role)
-                            {{ $role->display_name }}<br/>
-                        @endforeach
-                    </td>
-                </tr>
-                <tr>
-                    <td class="text-md-right">註冊時間：</td>
-                    <td>{{ $user->register_at }}</td>
-                </tr>
-                <tr>
-                    <td class="text-md-right">註冊IP：</td>
-                    <td>{{ $user->register_ip }}</td>
-                </tr>
-                <tr>
-                    <td class="text-md-right">最後登入時間：</td>
-                    <td>{{ $user->last_login_at }}</td>
-                </tr>
-                <tr>
-                    <td class="text-md-right">最後登入IP：</td>
-                    <td>{{ $user->last_login_ip }}</td>
-                </tr>
-            </table>
-        </div>
-        @if($user->is_local_account)
-            <div class="card-block text-center">
-                <a href="{{ route('profile.edit') }}" class="btn btn-primary">編輯資料</a>
-                <a href="{{ route('password.change') }}" class="btn btn-primary">修改密碼</a>
             </div>
-        @endif
+        </div>
     </div>
 @endsection
