f22889a5d0e889d53542aa1c45056e8fb4930d4b
KID <jyhsu2000@yahoo.com.tw>
2017-07-14 17:45:45 +0800

    Create student manage permission

---
+++ database/migrations/2017_07_14_174335_create_student_manage_permission.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Permission;
+use App\Role;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentManagePermission extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $permStudentManage = Permission::create([
+            'name'         => 'student.manage',
+            'display_name' => '管理學生',
+            'description'  => '查看學生資料、新增學生資料等',
+        ]);
+
+        /* @var Role $admin */
+        $admin = Role::where('name', 'Admin')->first();
+        $admin->attachPermission($permStudentManage);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Permission::where('name', 'student.manage')->delete();
+    }
+}
