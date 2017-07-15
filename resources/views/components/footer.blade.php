7c000a0239dac39bb427934338a1f41fdbbce876
Chan, Danny <danny50610@yahoo.com.tw>
2017-07-15 20:51:32 +0800

    Change login link color

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -10,9 +10,9 @@
             {{--<li>{!! HTML::linkRoute('staff', '工作人員') !!}</li>--}}
             {{--<li><a href="mailto:{{ urlencode('"駭客出任務"') }}<ics@mail.fcu.edu.tw>" target="_blank"><span class="glyphicon glyphicon-envelope" aria-hidden="true" style="margin-right: 5px;"></span>聯絡我們</a></li>--}}
             @if(Auth::guest())
-                <li class="breadcrumb-item"  style="color: #636c72">
+                <li class="breadcrumb-item">
                     {!! link_to_route('login', '管理員登入', [], [
-                        'style' => 'color: #f5f5f5;',
+                        'style' => 'color: #636c72;',
                     ]) !!}
                 </li>
             @endif
