1b3feaa5f1fedfaf88978e198d8f685020aea9b2
KID <jyhsu2000@yahoo.com.tw>
2017-07-17 00:09:36 +0800

    Prevent code of Qrcode similar to NID

--- app/Observers/QrcodeObserver.php
+++ app/Observers/QrcodeObserver.php
@@ -9,8 +9,11 @@ class QrcodeObserver
 {
     public function creating(Qrcode $qrcode)
     {
-        //若code為空，或該code已存在
-        while (empty($qrcode->code) || Qrcode::where('code', $qrcode->code)->count() != 0) {
+        //若code為空、該code已存在、code太像NID等情況
+        while (empty($qrcode->code)
+            || Qrcode::where('code', $qrcode->code)->count() != 0
+            || preg_match('#^[a-zA-Z]\d+$#', $qrcode->code)
+        ) {
             //隨機產生長度8的大寫英文數字字串
             $code = strtoupper(str_random(8));
             $qrcode->code = $code;
