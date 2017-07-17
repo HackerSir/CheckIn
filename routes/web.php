605bd62d656e6565e12d4d7f0f3c4fdbdda5f587
KID <jyhsu2000@yahoo.com.tw>
2017-07-17 20:18:42 +0800

    Make qrcode and barcode picture can only access by auth user

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,6 @@ Route::group(['prefix' => 'oauth', 'namespace' => 'Auth'], function () {
     Route::any('login', 'OAuthController@login')->name('oauth.login');
 });
 
-//條碼圖
-Route::group(['prefix' => 'code'], function () {
-    Route::get('qrcode/{code}', 'CodePictureController@qrcode')->name('code-picture.qrcode');
-    Route::get('barcode/{code}', 'CodePictureController@barcode')->name('code-picture.barcode');
-});
-
 //服務條款(含隱私權跟免責)
 Route::get('terms', function () {
     return view('misc.terms');
@@ -91,6 +85,11 @@ Route::group(['middleware' => ['auth', 'email']], function () {
     });
     //QR Code 掃描
     Route::get('qr/{code}', 'QrcodeScanController@scan')->name('qrcode.scan');
+    //條碼圖
+    Route::group(['prefix' => 'code'], function () {
+        Route::get('qrcode/{code}', 'CodePictureController@qrcode')->name('code-picture.qrcode');
+        Route::get('barcode/{code}', 'CodePictureController@barcode')->name('code-picture.barcode');
+    });
 
     //會員資料
     Route::group(['prefix' => 'profile'], function () {
