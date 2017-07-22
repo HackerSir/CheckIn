503d7ffc4b86594c88639a48956f822d776cf192
KID <jyhsu2000@yahoo.com.tw>
2017-07-22 18:30:52 +0800

    Fix places of longitude and latitude

--- database/migrations/2017_07_10_170202_create_booths_table.php
+++ database/migrations/2017_07_10_170202_create_booths_table.php
@@ -16,8 +16,8 @@ class CreateBoothsTable extends Migration
             $table->increments('id');
             $table->unsignedInteger('club_id')->nullable()->comment('對應社團');
             $table->string('name')->comment('名稱');
-            $table->float('longitude')->nullable()->comment('經度');
-            $table->float('latitude')->nullable()->comment('緯度');
+            $table->float('longitude', 10, 7)->nullable()->comment('經度');
+            $table->float('latitude', 10, 7)->nullable()->comment('緯度');
             $table->timestamps();
 
             $table->foreign('club_id')->references('id')->on('clubs')
