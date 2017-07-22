7829d8c02b91d52bec7bc2273607884f6c7e9039
KID <jyhsu2000@yahoo.com.tw>
2017-07-22 15:39:13 +0800

    Create setting manage permission

---
+++ database/migrations/2017_07_22_153801_create_setting_manage_permission.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Permission;
+use App\Role;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingManagePermission extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $permSettingManage = Permission::create([
+            'name'         => 'setting.manage',
+            'display_name' => '管理設定',
+            'description'  => '調整網站設定',
+        ]);
+
+        /* @var Role $admin */
+        $admin = Role::where('name', 'Admin')->first();
+        $admin->attachPermission($permSettingManage);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Permission::where('name', 'setting.manage')->delete();
+    }
+}
