bb4e846e8351f73691bfdb2611e560182c37d915
KID <jyhsu2000@yahoo.com.tw>
2017-07-28 19:41:56 +0800

    Add some TODO

--- app/Http/Controllers/QrcodeScanController.php
+++ app/Http/Controllers/QrcodeScanController.php
@@ -14,9 +14,16 @@ class QrcodeScanController extends Controller
      */
     public function scan($code)
     {
+        //TODO:檢查是否屬於活動時間
+
         //找出 QR Code
         $qrcode = Qrcode::where('code', $code)->first();
-        //TODO
+
+        //TODO:檢查QR Code是否已經被學生綁定
+        //TODO:檢查掃描使用者是否為攤位負責人
+        //TODO:檢查QR Code最後一組QR Code
+        //TODO:檢查是否在該攤位重複打卡
+        //TODO:打卡
         dd($code, $qrcode);
     }
 }
