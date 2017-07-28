82a21de692c3eea41e45b438575c9268be95d365
KID <jyhsu2000@yahoo.com.tw>
2017-07-28 20:51:10 +0800

    Update scan process

--- app/Http/Controllers/QrcodeScanController.php
+++ app/Http/Controllers/QrcodeScanController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Qrcode;
+use Carbon\Carbon;
+use Setting;
 
 class QrcodeScanController extends Controller
 {
@@ -14,14 +16,35 @@ class QrcodeScanController extends Controller
      */
     public function scan($code)
     {
-        //TODO:檢查是否屬於活動時間
-
         //找出 QR Code
+        /** @var Qrcode $qrcode */
         $qrcode = Qrcode::where('code', $code)->first();
+        if (!$qrcode) {
+            dd('QR Code無效');
+        }
 
-        //TODO:檢查QR Code是否已經被學生綁定
+        //檢查QR Code是否已經被學生綁定
+        if (!$qrcode->student) {
+            dd('該QR Code不屬於任何學生');
+        }
+
+        //檢查是否屬於活動時間
+        $startAt = new Carbon(Setting::get('start_at'));
+        if ($startAt->gte(Carbon::now())) {
+            dd('活動尚未開始');
+        }
+        $endAt = new Carbon(Setting::get('end_at'));
+        if ($endAt->lte(Carbon::now())) {
+            dd('活動已經結束');
+        }
+
         //TODO:檢查掃描使用者是否為攤位負責人
-        //TODO:檢查QR Code最後一組QR Code
+
+        //檢查QR Code最後一組QR Code
+        if (!$qrcode->is_last_one) {
+            dd('非最後一組 QR Code');
+        }
+
         //TODO:檢查是否在該攤位重複打卡
         //TODO:打卡
         dd($code, $qrcode);
