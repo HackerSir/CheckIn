0fda5c709d1250f2787fcb1ae0921ed2af6ab58e
KID <jyhsu2000@yahoo.com.tw>
2017-08-01 00:26:59 +0800

    Create api-key:reset command to reset count of all ApiKey

---
+++ app/Console/Commands/ResetApiKeyCount.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\ApiKey;
+use Illuminate\Console\Command;
+
+class ResetApiKeyCount extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'api-key:reset';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Reset count of all ApiKey';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        ApiKey::query()->update(['count' => 0]);
+        $this->info('Counts of all ApiKey have been reset.');
+    }
+}
