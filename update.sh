5f468910d2a49e0c749094041da45c13e7165105
Chan, Danny <danny50610@yahoo.com.tw>
2017-07-31 15:53:50 +0800

    Add update.sh

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# 切換到腳本所在路徑，防止腳本在其他路徑被執行
+cd "$(dirname "$0")"
+# 清除之前sudo留下的有效時間，強制重新輸入密碼
+sudo -k
+# 顯示執行指令
+set -x
+# 自動更新專案
+sudo git pull
+sudo composer install
+sudo php artisan migrate
+sudo chown www-data:www-data -R .
