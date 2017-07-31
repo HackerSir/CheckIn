ef0b2f9ec6d4ef914223e8504bb82bad9f73d4bc
Chan, Danny <danny50610@yahoo.com.tw>
2017-07-31 17:14:21 +0800

    Update index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,21 +4,17 @@
 
 @section('css')
     <style>
-        .jumbotron {
-            text-align: center;
+        #main_jumbotron {
             word-break: break-all;
-            background: rgba(100, 100, 100, .6);
-            margin-top: 20vh;
             margin-bottom: 0;
-            padding-top: 40px;
             border-radius: 20px;
         }
     </style>
 @endsection
 @section('content')
-    <div class="jumbotron">
-        <h1 class="display-1">{{ config('app.name') }}</h1>
-        <h2 class="display-3">逢甲大學黑客社</h2>
-        <a href="javascript:void(0)" class="btn btn-primary btn-lg" style="margin-top: 5vh;" title="Let's GO!!">GO!</a>
+    <div id="main_jumbotron" class="jumbotron mt-3">
+        <h1 class="display-2">{{ config('app.cht_name') }}</h1>
+        <h2>2017 逢甲社團博覽會集點打卡</h2>
+        <a href="{{ route('oauth.index') }}" class="btn btn-primary btn-lg" title="Let's GO!!">GO!</a>
     </div>
 @endsection
