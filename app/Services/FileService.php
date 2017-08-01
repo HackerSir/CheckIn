a18f5aea22805568b7746baa7b33fde576bca0f8
KID <jyhsu2000@yahoo.com.tw>
2017-08-01 10:12:19 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -49,11 +49,11 @@ class FileService extends Service
         //QR Code
         $scanUrl = route('qrcode.scan', $qrcode->code);
         $qrcodePath = DNS2D::getBarcodePNGPath($scanUrl, 'QRCODE', 18, 18);
-        $section->addImage($qrcodePath, ['alignment' => Jc::CENTER,]);
+        $section->addImage($qrcodePath, ['alignment' => Jc::CENTER]);
         $section->addTextBreak(1, ['size' => 8], ['alignment' => Jc::CENTER]);
         //BarCode
         $BarCodePath = DNS1D::getBarcodePNGPath($qrcode->code, 'C128B', 4, 90);
-        $section->addImage($BarCodePath, ['alignment' => Jc::CENTER,]);
+        $section->addImage($BarCodePath, ['alignment' => Jc::CENTER]);
         //Code
         $section->addText($qrcode->code, ['size' => 66], ['alignment' => Jc::CENTER]);
 
