9ddc463a357e32de364f5e568ecee6fc016de7c6
KID <jyhsu2000@yahoo.com.tw>
2017-08-02 17:01:29 +0800

    Extract mail confirmed condition

--- app/Http/Middleware/LaravelMenu.php
+++ app/Http/Middleware/LaravelMenu.php
@@ -31,67 +31,68 @@ class LaravelMenu
                 if (!$user->is_confirmed) {
                     $menu->add('尚未完成信箱驗證', ['route' => 'confirm-mail.resend'])
                         ->link->attr(['class' => 'text-danger']);
-                }
-                //活動選單
-                if (Laratrust::can('activity-menu.view') and $user->isConfirmed) {
-                    /** @var \Lavary\Menu\Builder $activityMenu */
-                    $activityMenu = $menu->add('活動選單', 'javascript:void(0)');
+                } else {
+                    //活動選單
+                    if (Laratrust::can('activity-menu.view')) {
+                        /** @var \Lavary\Menu\Builder $activityMenu */
+                        $activityMenu = $menu->add('活動選單', 'javascript:void(0)');
 
-                    if (Laratrust::can('student.manage')) {
-                        $activityMenu->add('學生管理', ['route' => 'student.index'])->active('student/*');
+                        if (Laratrust::can('student.manage')) {
+                            $activityMenu->add('學生管理', ['route' => 'student.index'])->active('student/*');
+                        }
+
+                        if (Laratrust::can('qrcode.manage')) {
+                            $activityMenu->add('QR Code', ['route' => 'qrcode.index'])->active('qrcode/*');
+                            $activityMenu->add('QR Code 集', ['route' => 'qrcode-set.index'])->active('qrcode-set/*');
+                        }
+
+                        if (Laratrust::can('booth.manage')) {
+                            $activityMenu->add('攤位管理', ['route' => 'booth.index'])->active('booth/*');
+                        }
+
+                        if (Laratrust::can('club-type.manage')) {
+                            $activityMenu->add('社團類型管理', ['route' => 'club-type.index'])->active('club-type/*');
+                        }
+
+                        if (Laratrust::can('club.manage')) {
+                            $activityMenu->add('社團管理', ['route' => 'club.index'])->active('club/*');
+                        }
+
+                        if (Laratrust::can('record.manage')) {
+                            $activityMenu->add('打卡紀錄管理', ['route' => 'record.index'])->active('record/*');
+                        }
+
+                        if (Laratrust::can('ticket.manage')) {
+                            $activityMenu->add('抽獎編號管理', ['route' => 'ticket.index'])->active('ticket/*');
+                        }
+
+                        if (Laratrust::can('setting.manage')) {
+                            $activityMenu->add('活動設定', ['route' => 'setting.edit']);
+                        }
                     }
+                    //管理員
+                    if (Laratrust::can('menu.view')) {
+                        /** @var \Lavary\Menu\Builder $adminMenu */
+                        $adminMenu = $menu->add('管理選單', 'javascript:void(0)');
 
-                    if (Laratrust::can('qrcode.manage')) {
-                        $activityMenu->add('QR Code', ['route' => 'qrcode.index'])->active('qrcode/*');
-                        $activityMenu->add('QR Code 集', ['route' => 'qrcode-set.index'])->active('qrcode-set/*');
-                    }
+                        if (Laratrust::can(['user.manage', 'user.view'])) {
+                            $adminMenu->add('會員清單', ['route' => 'user.index'])->active('user/*');
+                        }
 
-                    if (Laratrust::can('booth.manage')) {
-                        $activityMenu->add('攤位管理', ['route' => 'booth.index'])->active('booth/*');
-                    }
+                        if (Laratrust::can('role.manage')) {
+                            $adminMenu->add('角色管理', ['route' => 'role.index'])->active('role/*');
+                        }
 
-                    if (Laratrust::can('club-type.manage')) {
-                        $activityMenu->add('社團類型管理', ['route' => 'club-type.index'])->active('club-type/*');
-                    }
+                        if (Laratrust::can('api-key.manage')) {
+                            $adminMenu->add('API Key 管理', ['route' => 'api-key.index'])->active('api-key/*');
+                        }
 
-                    if (Laratrust::can('club.manage')) {
-                        $activityMenu->add('社團管理', ['route' => 'club.index'])->active('club/*');
-                    }
-
-                    if (Laratrust::can('record.manage')) {
-                        $activityMenu->add('打卡紀錄管理', ['route' => 'record.index'])->active('record/*');
-                    }
-
-                    if (Laratrust::can('ticket.manage')) {
-                        $activityMenu->add('抽獎編號管理', ['route' => 'ticket.index'])->active('ticket/*');
-                    }
-
-                    if (Laratrust::can('setting.manage')) {
-                        $activityMenu->add('活動設定', ['route' => 'setting.edit']);
-                    }
-                }
-                //管理員
-                if (Laratrust::can('menu.view') and $user->isConfirmed) {
-                    /** @var \Lavary\Menu\Builder $adminMenu */
-                    $adminMenu = $menu->add('管理選單', 'javascript:void(0)');
-
-                    if (Laratrust::can(['user.manage', 'user.view'])) {
-                        $adminMenu->add('會員清單', ['route' => 'user.index'])->active('user/*');
-                    }
-
-                    if (Laratrust::can('role.manage')) {
-                        $adminMenu->add('角色管理', ['route' => 'role.index'])->active('role/*');
-                    }
-
-                    if (Laratrust::can('api-key.manage')) {
-                        $adminMenu->add('API Key 管理', ['route' => 'api-key.index'])->active('api-key/*');
-                    }
-
-                    if (Laratrust::can('log-viewer.access')) {
-                        $adminMenu->add(
-                            '記錄檢視器 <i class="fa fa-external-link" aria-hidden="true"></i>',
-                            ['route' => 'log-viewer::dashboard']
-                        )->link->attr('target', '_blank');
+                        if (Laratrust::can('log-viewer.access')) {
+                            $adminMenu->add(
+                                '記錄檢視器 <i class="fa fa-external-link" aria-hidden="true"></i>',
+                                ['route' => 'log-viewer::dashboard']
+                            )->link->attr('target', '_blank');
+                        }
                     }
                 }
                 /** @var \Lavary\Menu\Builder $userMenu */
