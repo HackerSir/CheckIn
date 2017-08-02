694a8ef87f57abf6a5ea8c1b341c565e3a6bccd4
Chan, Danny <danny50610@yahoo.com.tw>
2017-08-02 15:19:23 +0800

    Remove unused text

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta property="og:title" content="@yield('title')::{{ config('app.name') }}">
     <meta property="og:url" content="{{ URL::current() }}">
     <meta property="og:image" content="{{ asset('img/hacker.png') }}">
-    <meta property="og:description" content="{{ config('app.name') }} - Laravel基礎專案">
+    <meta property="og:description" content="{{ config('app.name') }}">
 
     <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
     <link rel="icon" type="image/ico" href="{{ asset('favicon.ico') }}">
