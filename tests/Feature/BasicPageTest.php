9f1ac13c005f220a30cf25ed05f715ba685a1656
Chan, Danny <danny50610@yahoo.com.tw>
2017-08-02 15:49:40 +0800

    Rename test class name

--- tests/Feature/BasicPageTest.php
+++ tests/Feature/BasicPageTest.php
@@ -4,14 +4,14 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class BasicPageTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testHomePage()
     {
         $response = $this->get('/');
 
