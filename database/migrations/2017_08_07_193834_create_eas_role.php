11da08a4b3a1a00c5d6cf7d2e86cded22ebfd7c8
Chan, Danny <danny50610@yahoo.com.tw>
2017-08-07 20:07:47 +0800

    Create eas role

---
+++ database/migrations/2017_08_07_193834_create_eas_role.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Permission;
+use App\Role;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEasRole extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $eas = Role::create([
+            'name'         => 'EAS',
+            'display_name' => '課外活動組',
+            'description'  => '擁有活動權限的管理者',
+        ]);
+
+        $permissionsList = [
+            'student.manage',
+            'qrcode.manage',
+            'activity-menu.view',
+            'booth.manage',
+            'club.manage',
+            'setting.manage',
+            'club-type.manage',
+            'record.manage',
+            'ticket.manage',
+            'extra-ticket.manage',
+        ];
+
+        $eas->attachPermissions(Permission::whereIn('name', $permissionsList)->get());
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Role::where('name', 'EAS')->delete();
+    }
+}
