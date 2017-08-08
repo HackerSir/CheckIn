16f8fedf3206613654ed204d2324a79776e084ef
Chan, Danny <danny50610@yahoo.com.tw>
2017-08-08 13:58:01 +0800

    Change page css

--- resources/views/qrcode-set/index.blade.php
+++ resources/views/qrcode-set/index.blade.php
@@ -7,9 +7,9 @@
         <a href="{{ route('qrcode.index') }}" class="btn btn-secondary mb-2">
             <i class="fa fa-arrow-left" aria-hidden="true"></i> QR Code
         </a>
+        <h1>QR Code 集</h1>
         <div class="card mt-1">
             <div class="card-block">
-                <h1>QR Code 集</h1>
                 <p>批次建立QR Code</p>
                 <a href="{{ route('qrcode-set.create') }}" class="btn btn-primary">
                     <i class="fa fa-plus-circle" aria-hidden="true"></i> 新增 QR Code 集
