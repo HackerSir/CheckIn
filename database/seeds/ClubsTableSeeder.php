3f22068d08ce271520263e08ca9d08e71d2046eb
Chan, Danny <danny50610@yahoo.com.tw>
2017-08-08 15:11:51 +0800

    Add ClubsTableSeeder

---
+++ database/seeds/ClubsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Club;
+use App\ClubType;
+use Illuminate\Database\Seeder;
+
+class ClubsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     * 建立多個社團與分類
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\ClubType::class, 8)->create();
+
+        factory(App\Club::class, 50)->create()->each(function (Club $club) {
+            /** @var ClubType $clubType */
+            $clubType = ClubType::inRandomOrder()->first();
+
+            $clubType->clubs()->save($club);
+        });
+    }
+}
