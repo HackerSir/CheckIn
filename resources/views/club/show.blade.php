dcdfb768ea4e0b85bfa468de4b914cc1b3aa4a0e
Chan, Danny <danny50610@yahoo.com.tw>
2017-08-10 20:51:17 +0800

    [WIP] Change club show page

--- resources/views/club/show.blade.php
+++ resources/views/club/show.blade.php
@@ -11,80 +11,71 @@
 @endsection
 
 @section('content')
-    <div class="row mt-3 pb-3">
-        <div class="col-md-8 offset-md-2">
-            <a href="{{ route('club.index') }}" class="btn btn-secondary mb-2">
+    <div class="mt-3 pb-3">
+        <div class="mb-2">
+            <a href="{{ route('club.index') }}" class="btn btn-secondary">
                 <i class="fa fa-arrow-left" aria-hidden="true"></i> 社團管理
             </a>
-            <h1>{{ $club->name }} - 社團</h1>
-            <div class="card">
-                @if($club->imgurImage)
-                    <div class="card-block text-center">
-                        <img src="{{ $club->imgurImage->thumbnail('h') }}" class="img-fluid">
+            <a href="{{ route('club.edit', $club) }}" class="btn btn-primary">編輯資料</a>
+            {!! Form::open(['route' => ['club.destroy', $club], 'style' => 'display: inline', 'method' => 'DELETE', 'onSubmit' => "return confirm('確定要刪除嗎？');"]) !!}
+            <button type="submit" class="btn btn-danger">刪除社團</button>
+            {!! Form::close() !!}
+        </div>
+        <div class="card">
+            <div class="card-block">
+                <h1>{{ $club->name }}</h1>
+                <div class="row">
+                    <div class="col-12 col-lg-5 mt-1">
+                        @if($club->imgurImage)
+                            <img src="{{ $club->imgurImage->thumbnail('l') }}" class="img-fluid">
+                        @endif
                     </div>
-                @endif
-                <div class="card-block">
-                    <table class="table table-hover">
-                        <tr>
-                            <td class="text-md-right">類型：</td>
-                            <td>{{ $club->clubType->name ?? '' }}</td>
-                        </tr>
-                        <tr>
-                            <td class="text-md-right">名稱：</td>
-                            <td>{{ $club->name }}</td>
-                        </tr>
-                        <tr>
-                            <td class="text-md-right">集點：</td>
-                            <td>
+                    <div class="col-12 col-lg-7 mt-1">
+                        <dl class="row" style="font-size: 120%">
+                            <dt class="col-sm-3">社團編號</dt>
+                            <dd class="col-sm-9">{{ $club->number }}</dd>
+
+                            <dt class="col-sm-3">社團類型</dt>
+                            <dd class="col-sm-9">{!! $club->clubType->tag ?? '' !!}</dd>
+
+                            <dt class="col-sm-3">可否集點</dt>
+                            <dd class="col-sm-9">
                                 @if($club->is_counted)
-                                    <i class="fa fa-check-square-o fa-2x" aria-hidden="true"></i>
+                                    <i class="fa fa-check-square-o" aria-hidden="true"></i>
                                 @else
-                                    <i class="fa fa-square-o fa-2x" aria-hidden="true"></i>
+                                    <i class="fa fa-square-o" aria-hidden="true"></i>
                                 @endif
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="text-md-right">編號：</td>
-                            <td>{{ $club->number }}</td>
-                        </tr>
-                        <tr>
-                            <td class="text-md-right">攤位：</td>
-                            <td>
+                            </dd>
+
+                            <dt class="col-sm-3">攤位</dt>
+                            <dd class="col-sm-9">
                                 @foreach($club->booths as $booth)
                                     {{ link_to_route('booth.show', $booth->name, $booth) }}<br/>
                                 @endforeach
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="text-md-right">網址：</td>
-                            <td>
+                            </dd>
+
+                            <dt class="col-sm-3">網站</dt>
+                            <dd class="col-sm-9">
                                 @if($club->url)
                                     {{ link_to($club->url, $club->url, ['target' => '_blank']) }}
                                 @endif
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="text-md-right">負責人：</td>
-                            <td>
+                            </dd>
+
+                            <dt class="col-sm-3">負責人</dt>
+                            <dd class="col-sm-9">
                                 @foreach($club->users as $user)
                                     {{ $user->name }}<br/>
                                 @endforeach
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                @if($club->description)
-                    <div class="card-block">
-                        {!! nl2br(e($club->description)) !!}
+                            </dd>
+                        </dl>
                     </div>
-                @endif
-                <div class="card-block text-center">
-                    <a href="{{ route('club.edit', $club) }}" class="btn btn-primary">編輯資料</a>
-                    {!! Form::open(['route' => ['club.destroy', $club], 'style' => 'display: inline', 'method' => 'DELETE', 'onSubmit' => "return confirm('確定要刪除嗎？');"]) !!}
-                    <button type="submit" class="btn btn-danger">刪除社團</button>
-                    {!! Form::close() !!}
+                </div>
+                <div class="mt-1">
+                    <h2>簡介</h2>
+                    <p style="font-size: 120%">{!! nl2br(e($club->description)) !!}</p>
                 </div>
             </div>
+
         </div>
     </div>
 @endsection
