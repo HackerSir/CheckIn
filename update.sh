da7a4ad29d082e8d75f5c70722f4fa0e410e87d6
KID <jyhsu2000@yahoo.com.tw>
2017-08-22 11:30:34 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
+# 顯示執行指令 && 發生錯誤時會停止
+set -xe
 # 切換到腳本所在路徑，防止腳本在其他路徑被執行
 cd "$(dirname "$0")"
 # 清除之前sudo留下的有效時間，強制重新輸入密碼
 sudo -k
-# 顯示執行指令
-set -x
 # 自動更新專案
 sudo git pull
 sudo composer install --no-dev
