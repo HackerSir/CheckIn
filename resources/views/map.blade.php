91778214347b6b7a9554027b0c82bc08adbf3797
KID <jyhsu2000@yahoo.com.tw>
2017-09-01 14:57:41 +0800

    Disable google map js in static map page

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -44,52 +44,54 @@
 @endsection
 
 @section('js')
-    <script>
-        function initMap() {
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 19,
-                center: {lat: 24.179976, lng: 120.648279}
-            });
+    @if ($type == 'google')
+        <script>
+            function initMap() {
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 19,
+                    center: {lat: 24.179976, lng: 120.648279}
+                });
 
-            var myloc = new google.maps.Marker({
-                clickable: false,
-                icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
-                    new google.maps.Size(22, 22),
-                    new google.maps.Point(0, 18),
-                    new google.maps.Point(11, 11)),
-                shadow: null,
-                zIndex: 999,
-                map: map
-            });
+                var myloc = new google.maps.Marker({
+                    clickable: false,
+                    icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+                        new google.maps.Size(22, 22),
+                        new google.maps.Point(0, 18),
+                        new google.maps.Point(11, 11)),
+                    shadow: null,
+                    zIndex: 999,
+                    map: map
+                });
 
-            if (navigator.geolocation) navigator.geolocation.getCurrentPosition(function (pos) {
-                var me = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
-                myloc.setPosition(me);
-            }, function (error) {
-                // ...
-            });
+                if (navigator.geolocation) navigator.geolocation.getCurrentPosition(function (pos) {
+                    var me = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+                    myloc.setPosition(me);
+                }, function (error) {
+                    // ...
+                });
 
-            var rectangle = new google.maps.Rectangle({
-                strokeColor: '#0000FF',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: '#0000FF',
-                fillOpacity: 0.35,
-                map: map,
-                bounds: {
-                    north: 24.179976 + 0.00002,
-                    south: 24.179976 - 0.00002,
-                    east: 120.648279 + 0.00002,
-                    west: 120.648279 - 0.00002
-                }
-            });
+                var rectangle = new google.maps.Rectangle({
+                    strokeColor: '#0000FF',
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: '#0000FF',
+                    fillOpacity: 0.35,
+                    map: map,
+                    bounds: {
+                        north: 24.179976 + 0.00002,
+                        south: 24.179976 - 0.00002,
+                        east: 120.648279 + 0.00002,
+                        west: 120.648279 - 0.00002
+                    }
+                });
 
-            rectangle.addListener('click', function () {
-                window.open('https://www.google.com', '_blank');
-            });
-        }
-    </script>
-    <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key={{ GoogleApi::getKey() }}&callback=initMap">
-    </script>
+                rectangle.addListener('click', function () {
+                    window.open('https://www.google.com', '_blank');
+                });
+            }
+        </script>
+        <script async defer
+                src="https://maps.googleapis.com/maps/api/js?key={{ GoogleApi::getKey() }}&callback=initMap">
+        </script>
+    @endif
 @endsection
