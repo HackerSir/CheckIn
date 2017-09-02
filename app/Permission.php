c9b566bccb08248af72ad68014c86e63bde5b826
KID <jyhsu2000@yahoo.com.tw>
2017-09-02 11:38:54 +0800

    Regenerate phpDoc

--- app/Permission.php
+++ app/Permission.php
@@ -9,17 +9,17 @@ use Laratrust\LaratrustPermission;
  *
  * @property int $id
  * @property string $name
- * @property string $display_name
- * @property string $description
- * @property \Carbon\Carbon $created_at
- * @property \Carbon\Carbon $updated_at
+ * @property string|null $display_name
+ * @property string|null $description
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Role[] $roles
- * @method static \Illuminate\Database\Query\Builder|\App\Permission whereCreatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Permission whereDescription($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Permission whereDisplayName($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Permission whereId($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Permission whereName($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Permission whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Permission whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Permission whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Permission whereDisplayName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Permission whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Permission whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Permission whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class Permission extends LaratrustPermission
--- app/Role.php
+++ app/Role.php
@@ -9,19 +9,19 @@ use Laratrust\LaratrustRole;
  *
  * @property int $id
  * @property string $name
- * @property string $display_name
- * @property string $description
- * @property bool $protection
- * @property \Carbon\Carbon $created_at
- * @property \Carbon\Carbon $updated_at
+ * @property string|null $display_name
+ * @property string|null $description
+ * @property int $protection
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Permission[] $permissions
- * @method static \Illuminate\Database\Query\Builder|\App\Role whereCreatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Role whereDescription($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Role whereDisplayName($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Role whereId($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Role whereName($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Role whereProtection($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Role whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role whereDisplayName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role whereProtection($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class Role extends LaratrustRole
