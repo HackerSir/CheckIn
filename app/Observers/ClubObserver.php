31ad8b5369c03bc039237a7eaf5a17ee7f736664
KID <jyhsu2000@yahoo.com.tw>
2017-09-05 01:39:55 +0800

    Log updating of club

--- app/Observers/ClubObserver.php
+++ app/Observers/ClubObserver.php
@@ -3,6 +3,8 @@
 namespace App\Observers;
 
 use App\Club;
+use App\Services\LogService;
+use App\User;
 
 class ClubObserver
 {
@@ -12,5 +14,20 @@ class ClubObserver
         if ($club->imgurImage) {
             $club->imgurImage->delete();
         }
+    }
+
+    public function updating(Club $club)
+    {
+        /** @var User $user */
+        $user = auth()->user();
+        if (!$user) {
+            return;
+        }
+        $logService = app(LogService::class);
+        $logService->info(
+            "[Club][Update] {$user->name} 更新了 {$club->name} 的資料",
+            $club->getOriginal(),
+            $club->getAttributes()
+        );
     }
 }
