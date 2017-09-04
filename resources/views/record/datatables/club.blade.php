b732f32dff7a6f8578262fdaac1c8f9d401cf7d7
KID <jyhsu2000@yahoo.com.tw>
2017-09-04 19:02:24 +0800

    Add link to record list

--- resources/views/record/datatables/club.blade.php
+++ resources/views/record/datatables/club.blade.php
@@ -1,1 +1,9 @@
-{!! $record->club->display_name ?? '' !!}
+@if($record->club)
+    @if(Laratrust::can('club.manage'))
+        <a href="{{ route('club.show', $record->club) }}">
+            {!! $record->club->display_name !!}
+        </a>
+    @else
+        {!! $record->club->display_name !!}
+    @endif
+@endif
--- resources/views/record/datatables/student.blade.php
+++ resources/views/record/datatables/student.blade.php
@@ -1,1 +1,9 @@
-{{ $record->student->display_name ?? '' }}
+@if($record->student)
+    @if(Laratrust::can('student.manage'))
+        <a href="{{ route('student.show', $record->student) }}">
+            {{ $record->student->display_name }}
+        </a>
+    @else
+        {{ $record->student->display_name }}
+    @endif
+@endif
