ae88352b88511fd3f06f27c47da3393ada8da4a6
KID <jyhsu2000@yahoo.com.tw>
2017-09-07 14:09:06 +0800

    Log club create and delete

--- app/Observers/ClubObserver.php
+++ app/Observers/ClubObserver.php
@@ -14,6 +14,30 @@ class ClubObserver
         if ($club->imgurImage) {
             $club->imgurImage->delete();
         }
+        /** @var User $user */
+        $user = auth()->user();
+        if (!$user) {
+            return;
+        }
+        $logService = app(LogService::class);
+        $logService->info(
+            "[Club][Delete] {$user->name} 刪除了 {$club->name}",
+            $club->getAttributes()
+        );
+    }
+
+    public function creating(Club $club)
+    {
+        /** @var User $user */
+        $user = auth()->user();
+        if (!$user) {
+            return;
+        }
+        $logService = app(LogService::class);
+        $logService->info(
+            "[Club][Create] {$user->name} 建立了 {$club->name}",
+            $club->getAttributes()
+        );
     }
 
     public function updating(Club $club)
