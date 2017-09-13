8eb94f478dd4780dbed806c73d47bf120e1ae1a4
KID <jyhsu2000@yahoo.com.tw>
2017-09-13 13:08:14 +0800

    Extract progress bar

---
+++ resources/views/components/progress-bar.blade.php
@@ -0,0 +1,18 @@
+@for($i = 0; $i < floor($progress / 100); $i++)
+    <div class="progress w-80 mb-2">
+        <div class="progress-bar d-flex align-items-center justify-content-center"
+             role="progressbar" style="width: 100%;">
+            <div>{{ $progress }}%</div>
+        </div>
+    </div>
+@endfor
+@if($progress == 0 || $progress % 100 > 0)
+    <div class="progress w-80">
+        <div class="progress-bar d-flex align-items-center justify-content-center"
+             role="progressbar"
+             style="width: {{ $progress % 100 }}%; min-width: 3rem;"
+             aria-valuenow="{{ $progress % 100 }}" aria-valuemin="0" aria-valuemax="100">
+            <div>{{ $progress }}%</div>
+        </div>
+    </div>
+@endif
--- resources/views/student/show.blade.php
+++ resources/views/student/show.blade.php
@@ -66,24 +66,7 @@
                         $progress = ($student->countedRecords->count() / \Setting::get('target')) * 100;
                         $progress = round($progress, 2);
                     @endphp
-                    @for($i = 0; $i < floor($progress / 100); $i++)
-                        <div class="progress w-80 mb-2">
-                            <div class="progress-bar d-flex align-items-center justify-content-center"
-                                 role="progressbar" style="width: 100%;">
-                                <div>{{ $progress }}%</div>
-                            </div>
-                        </div>
-                    @endfor
-                    @if($progress % 100 > 0)
-                        <div class="progress w-80">
-                            <div class="progress-bar d-flex align-items-center justify-content-center"
-                                 role="progressbar"
-                                 style="width: {{ $progress % 100 }}%; min-width: 3rem;"
-                                 aria-valuenow="{{ $progress % 100 }}" aria-valuemin="0" aria-valuemax="100">
-                                <div>{{ $progress }}%</div>
-                            </div>
-                        </div>
-                    @endif
+                    @include('components.progress-bar', compact('progress'))
 
                     <hr/>
 
