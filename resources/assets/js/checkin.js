9337f86a8305da9f5768e210f4f9b28929e63708
Chan, Danny <danny50610@yahoo.com.tw>
2018-07-30 21:29:53 +0800

    Split Js File

--- resources/assets/js/checkin.js
+++ resources/assets/js/checkin.js
@@ -1,13 +1,5 @@
-require('./vue');
-
-import Echo from "laravel-echo"
-
-window.io = require('socket.io-client');
-
-window.Echo = new Echo({
-    broadcaster: 'socket.io',
-    host: process.env.MIX_ECHO_HOST
-});
+require('./bootstrap');
+require('./bootstrap-echo');
 
 window.Echo.private('student.' + window.Laravel.student)
     .listen('CheckInSuccess', (e) => {
