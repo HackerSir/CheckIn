dbd9975db8791809ebb06142373383d5b045eff3
KID <jyhsu2000@yahoo.com.tw>
2018-07-31 18:14:34 +0800

    Fix navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,7 +7,7 @@
         <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.cht_name') }}</a>
 
         {{-- Item 會由 LaravelMenu 生成 --}}
-        <div class="collapse navbar-collapse" id="navbarResponsive">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             {{-- 左側選單 --}}
             <ul class="navbar-nav">
                 @include(config('laravel-menu.views.bootstrap-items'), array('items' => Menu::get('left')->roots()))
