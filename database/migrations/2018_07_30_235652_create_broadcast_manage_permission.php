03e3ff79037cd669e7c1f5ba276624dca222448b
KID <jyhsu2000@yahoo.com.tw>
2018-07-31 09:00:57 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- database/migrations/2018_07_30_235652_create_broadcast_manage_permission.php
+++ database/migrations/2018_07_30_235652_create_broadcast_manage_permission.php
@@ -2,8 +2,6 @@
 
 use App\Permission;
 use App\Role;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateBroadcastManagePermission extends Migration
