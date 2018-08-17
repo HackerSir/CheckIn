63ea8406dbbfab52cef7e35c538421a9ae3ef881
KID <jyhsu2000@yahoo.com.tw>
2018-08-17 16:37:28 +0800

    Update student.show

--- resources/views/student/show.blade.php
+++ resources/views/student/show.blade.php
@@ -62,7 +62,57 @@
 
             <hr/>
 
-            <h1>集點任務</h1>
+            <h1>抽獎活動</h1>
+            <div class="text-center">
+                @if(isset($student->ticket))
+                    <div class="alert alert-success">
+                        <i class="fas fa-ticket-alt"></i> 抽獎編號 <i class="fas fa-ticket-alt"></i>
+                        <h3>{{ sprintf("%04d", $student->ticket->id) }}</h3>
+                    </div>
+                @else
+                    @if(!$student->is_freshman)
+                        <div class="alert alert-danger">
+                            <i class="fas fa-exclamation-triangle"></i> 未具備抽獎資格，即使完成任務，也無法參加抽獎（抽獎活動限大學部新生參加）
+                        </div>
+                    @else
+                        <div class="alert alert-danger">
+                            <i class="fas fa-exclamation-triangle"></i> 完成以下任務，即可取得抽獎編號
+                        </div>
+                    @endif
+                @endif
+            </div>
+            <dl class="row" style="font-size: 120%">
+                <dt class="col-4 col-md-2">打卡集點</dt>
+                <dd class="col-8 col-md-10">
+                    @if($student->has_enough_counted_records)
+                        <span class="text-success">
+                            <i class="far fa-check-square"></i> 已完成
+                        </span>
+                    @else
+                        <span class="text-danger">
+                            <i class="far fa-square"></i> 未完成
+                        </span>
+                        <span>（{{ $student->countedRecords->count() }} / {{ \Setting::get('target') }}
+                            ）</span>
+                    @endif
+                </dd>
+                <dt class="col-4 col-md-2">填寫問卷</dt>
+                <dd class="col-8 col-md-10">
+                    @if($student->studentSurvey)
+                        <span class="text-success">
+                            <i class="far fa-check-square"></i> 已完成
+                        </span>
+                    @else
+                        <span class="text-danger">
+                            <i class="far fa-square"></i> 未完成
+                        </span>
+                    @endif
+                </dd>
+            </dl>
+
+            <hr/>
+
+            <h1>打卡集點</h1>
             <dl class="row" style="font-size: 120%">
                 <dt class="col-4 col-md-2">打卡次數</dt>
                 <dd class="col-8 col-md-10">{{ $student->records->count() }}</dd>
@@ -76,23 +126,6 @@
                 $progress = round($progress, 2);
             @endphp
             @include('components.progress-bar', compact('progress'))
-
-            <hr/>
-
-            <h1>抽獎編號</h1>
-            <div class="text-center">
-                @if(isset($student->ticket))
-                    <h3 class="text-danger">{{ sprintf("%04d", $student->ticket->id) }}</h3>
-                @else
-                    @if(!$student->is_freshman)
-                        <p class="text-danger">
-                            未具備抽獎資格，即使完成任務，也無法參加抽獎（抽獎活動限大學部新生參加）
-                        </p>
-                    @else
-                        <h3 class="text-danger">集點任務尚未完成</h3>
-                    @endif
-                @endif
-            </div>
 
             <hr/>
 
