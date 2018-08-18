b1c16a5eb6b6a101007e8e5a9d64a0729108bd49
Chan, Danny <danny50610@yahoo.com.tw>
2018-08-18 21:39:09 +0800

    為所欲為修改頁面

--- resources/views/components/record-list.blade.php
+++ resources/views/components/record-list.blade.php
@@ -1,34 +1,27 @@
 <ul class="list-group">
     @forelse($student->records as $record)
-        <li class="list-group-item">
-            <div>
-                <h4>{{ link_to_route('clubs.show', $record->club->name, $record->club) }}
-                    {!! $record->club->clubType->tag ?? '' !!}
-                    @if(!$record->club->is_counted)
-                        <span class='badge badge-secondary'>不列入集點</span>
-                    @endif
-                </h4>
-                <small>
-                    {{ $record->created_at }}
-                    （{{ (new \Carbon\Carbon($record->created_at))->diffForHumans() }}）
-                </small>
-                @if($showFeedbackButton ?? false)
-                    @if($student->feedback->contains('club_id', $record->club->id))
-                        @php($feedback = $student->feedback->filter(function($item)use($record){return $item->club_id == $record->club->id;})->first())
-                        <div class="float-md-right">
-                            <a href="{{ route('feedback.show', $feedback) }}" class="btn btn-success">
-                                <i class="fa fa-search"></i> 檢視回饋資料
-                            </a>
-                        </div>
-                    @else
-                        <div class="float-md-right">
-                            <a href="{{ route('feedback.create', $record->club) }}" class="btn btn-primary">
-                                <i class="fa fa-edit"></i> 填寫回饋資料
-                            </a>
-                        </div>
-                    @endif
+        <li class="list-group-item" >
+            <h4 class="mb-1">{{ link_to_route('clubs.show', $record->club->name, $record->club) }}
+                {!! $record->club->clubType->tag ?? '' !!}
+                @if(!$record->club->is_counted)
+                    <span class='badge badge-secondary'>不列入集點</span>
                 @endif
-            </div>
+            </h4>
+            <p>
+                <span title="{{ $record->created_at }}">{{ (new \Carbon\Carbon($record->created_at))->diffForHumans() }}</span>
+            </p>
+            @if($showFeedbackButton ?? false)
+                @if($student->feedback->contains('club_id', $record->club->id))
+                    @php($feedback = $student->feedback->filter(function($item)use($record){return $item->club_id == $record->club->id;})->first())
+                    <a href="{{ route('feedback.show', $feedback) }}" class="btn btn-success">
+                        <i class="fa fa-search"></i> 檢視回饋資料
+                    </a>
+                @else
+                    <a href="{{ route('feedback.create', $record->club) }}" class="btn btn-primary">
+                        <i class="fa fa-edit"></i> 填寫回饋資料
+                    </a>
+                @endif
+            @endif
         </li>
     @empty
         <li class="list-group-item">
--- resources/views/qrcode-scan/scan.blade.php
+++ resources/views/qrcode-scan/scan.blade.php
@@ -12,13 +12,13 @@
         <div class="card-body">
             <h1>QR Code</h1>
             <dl class="row" style="font-size: 120%">
-                <dt class="col-4 col-md-2">條碼</dt>
-                <dd class="col-8 col-md-10 code">{{ $code }}</dd>
+                <dt class="col-md-2">條碼</dt>
+                <dd class="col-md-10 code">{{ $code }}</dd>
 
                 @if(isset($qrcode))
                     @if($qrcode->student)
-                        <dt class="col-4 col-md-2">學生</dt>
-                        <dd class="col-8 col-md-10">{{ $qrcode->student->masked_display_name }}</dd>
+                        <dt class="col-md-2">學生</dt>
+                        <dd class="col-md-10">{{ $qrcode->student->masked_display_name }}</dd>
                     @endif
                 @endif
             </dl>
@@ -26,27 +26,25 @@
                 <hr/>
 
                 <h1>抽獎活動</h1>
-                <div class="text-center">
-                    @if(isset($qrcode->student->ticket))
-                        <div class="alert alert-success">
-                            <i class="fas fa-ticket-alt"></i> 抽獎編號 <i class="fas fa-ticket-alt"></i>
-                            <h3>{{ sprintf("%04d", $qrcode->student->ticket->id) }}</h3>
+                @if(isset($qrcode->student->ticket))
+                    <div class="alert alert-success">
+                        <i class="fas fa-ticket-alt"></i> 抽獎編號 <i class="fas fa-ticket-alt"></i>
+                        <h3>{{ sprintf("%04d", $qrcode->student->ticket->id) }}</h3>
+                    </div>
+                @else
+                    @if(!$qrcode->student->is_freshman)
+                        <div class="alert alert-warning">
+                            <i class="fas fa-exclamation-triangle"></i> 抽獎活動限<strong>大學部新生</strong>參加，即使完成任務，也無法參加抽獎
                         </div>
                     @else
-                        @if(!$qrcode->student->is_freshman)
-                            <div class="alert alert-danger">
-                                <i class="fas fa-exclamation-triangle"></i> 未具備抽獎資格，即使完成任務，也無法參加抽獎（抽獎活動限大學部新生參加）
-                            </div>
-                        @else
-                            <div class="alert alert-danger">
-                                <i class="fas fa-exclamation-triangle"></i> 完成以下任務，即可取得抽獎編號
-                            </div>
-                        @endif
+                        <div class="alert alert-info">
+                            <i class="fas fa-exclamation-triangle"></i> 完成以下任務，即可取得抽獎編號
+                        </div>
                     @endif
-                </div>
+                @endif
                 <dl class="row" style="font-size: 120%">
-                    <dt class="col-4 col-md-2">打卡集點</dt>
-                    <dd class="col-8 col-md-10">
+                    <dt class="col-md-2">打卡集點</dt>
+                    <dd class="col-md-10">
                         @if($qrcode->student->has_enough_counted_records)
                             <span class="text-success">
                                 <i class="far fa-check-square"></i> 已完成
@@ -59,8 +57,8 @@
                                 ）</span>
                         @endif
                     </dd>
-                    <dt class="col-4 col-md-2">填寫問卷</dt>
-                    <dd class="col-8 col-md-10">
+                    <dt class="col-md-2">填寫問卷</dt>
+                    <dd class="col-md-10">
                         @if($qrcode->student->studentSurvey)
                             <span class="text-success">
                                 <i class="far fa-check-square"></i> 已完成
@@ -76,19 +74,20 @@
                 <hr/>
 
                 <h1>打卡集點</h1>
-                <dl class="row" style="font-size: 120%">
-                    <dt class="col-4 col-md-2">打卡次數</dt>
-                    <dd class="col-8 col-md-10">{{ $qrcode->student->records->count() }}</dd>
-
-                    <dt class="col-4 col-md-2">進度</dt>
-                    <dd class="col-8 col-md-10">{{ $qrcode->student->countedRecords->count() }}
-                        / {{ \Setting::get('target') }}</dd>
-                </dl>
                 @php
                     $progress = ($qrcode->student->countedRecords->count() / \Setting::get('target')) * 100;
                     $progress = round($progress, 2);
                 @endphp
                 @include('components.progress-bar', compact('progress'))
+
+                <dl class="row" style="font-size: 120%">
+                    <dt class="col-md-2">打卡次數</dt>
+                    <dd class="col-md-10">{{ $qrcode->student->records->count() }}</dd>
+
+                    <dt class="col-md-2">進度</dt>
+                    <dd class="col-md-10">{{ $qrcode->student->countedRecords->count() }}
+                        / {{ \Setting::get('target') }}</dd>
+                </dl>
 
                 <hr/>
 
