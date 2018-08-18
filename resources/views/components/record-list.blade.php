67542e03be11594412fb00ec90684e5dc4a25370
Chan, Danny <danny50610@yahoo.com.tw>
2018-08-19 00:44:35 +0800

    當畫面夠寬時，將按鈕移至右邊

--- resources/views/components/record-list.blade.php
+++ resources/views/components/record-list.blade.php
@@ -1,29 +1,33 @@
 <ul class="list-group">
     @forelse($student->records as $record)
-        <li class="list-group-item">
-            <h4 class="mb-1">{{ link_to_route('clubs.show', $record->club->name, $record->club) }}
-                {!! $record->club->clubType->tag ?? '' !!}
-                @if(!$record->club->is_counted)
-                    <span class='badge badge-secondary'>不列入集點</span>
+        <li class="list-group-item clearfix">
+            <div class="float-xl-left">
+                <h4 class="mb-1">{{ link_to_route('clubs.show', $record->club->name, $record->club) }}
+                    {!! $record->club->clubType->tag ?? '' !!}
+                    @if(!$record->club->is_counted)
+                        <span class='badge badge-secondary'>不列入集點</span>
+                    @endif
+                </h4>
+                <p>
+                    <span title="{{ $record->created_at }}">
+                        {{ (new \Carbon\Carbon($record->created_at))->diffForHumans() }}
+                    </span>
+                </p>
+            </div>
+            <div class="float-xl-right">
+                @if($showFeedbackButton ?? false)
+                    @if($student->feedback->contains('club_id', $record->club->id))
+                        @php($feedback = $student->feedback->filter(function($item)use($record){return $item->club_id == $record->club->id;})->first())
+                        <a href="{{ route('feedback.show', $feedback) }}" class="btn btn-success">
+                            <i class="fa fa-search"></i> 檢視回饋資料
+                        </a>
+                    @else
+                        <a href="{{ route('feedback.create', $record->club) }}" class="btn btn-primary">
+                            <i class="fa fa-edit"></i> 填寫回饋資料
+                        </a>
+                    @endif
                 @endif
-            </h4>
-            <p>
-                <span title="{{ $record->created_at }}">
-                    {{ (new \Carbon\Carbon($record->created_at))->diffForHumans() }}
-                </span>
-            </p>
-            @if($showFeedbackButton ?? false)
-                @if($student->feedback->contains('club_id', $record->club->id))
-                    @php($feedback = $student->feedback->filter(function($item)use($record){return $item->club_id == $record->club->id;})->first())
-                    <a href="{{ route('feedback.show', $feedback) }}" class="btn btn-success">
-                        <i class="fa fa-search"></i> 檢視回饋資料
-                    </a>
-                @else
-                    <a href="{{ route('feedback.create', $record->club) }}" class="btn btn-primary">
-                        <i class="fa fa-edit"></i> 填寫回饋資料
-                    </a>
-                @endif
-            @endif
+            </div>
         </li>
     @empty
         <li class="list-group-item">
