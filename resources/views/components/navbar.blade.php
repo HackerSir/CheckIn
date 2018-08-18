de9b57522853d55768f933e62be96f0c1b5d8d3d
Chan, Danny <danny50610@yahoo.com.tw>
2018-08-18 17:34:32 +0800

    Fix bug: navbar 清單太長時，無法下拉到最下面

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md fixed-top navbar-light bg-light">
+<nav class="navbar navbar-expand-xl fixed-top navbar-light bg-light">
     <div class="container">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
