69314d903effe4af5bf1c9bdab03ae09be570cab
KID <jyhsu2000@yahoo.com.tw>
2018-08-31 00:17:26 +0800

    Update static map

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -34,13 +34,18 @@
 @section('main_content')
     @if ($type == 'static')
         <div class="mt-2">
-            <a href="https://i.imgur.com/PHKhyiI.jpg" target="_blank">
-                <img src="https://i.imgur.com/PHKhyiI.jpg" class="img-fluid" style="width: 100%">
+            <a href="https://i.imgur.com/7CK5yAn.jpg" target="_blank">
+                <img src="https://i.imgur.com/7CK5yAn.jpg" class="img-fluid w-100">
             </a>
         </div>
         <div class="mt-2">
-            <a href="https://i.imgur.com/pO5Fave.jpg" target="_blank">
-                <img src="https://i.imgur.com/pO5Fave.jpg" class="img-fluid" style="width: 100%">
+            <a href="https://i.imgur.com/WzyFs8Y.jpg" target="_blank">
+                <img src="https://i.imgur.com/WzyFs8Y.jpg" class="img-fluid w-100">
+            </a>
+        </div>
+        <div class="mt-2">
+            <a href="https://i.imgur.com/OAzF98q.jpg" target="_blank">
+                <img src="https://i.imgur.com/OAzF98q.jpg" class="img-fluid w-100">
             </a>
         </div>
     @elseif ($type == 'google')
