c500b828b163e94a7039e7ec40703b0e60e2629c
KID <jyhsu2000@yahoo.com.tw>
2018-08-31 16:36:08 +0800

    Extract some duplicate parts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,14 +2,6 @@
 
 @section('title', $student->name)
 
-@section('css')
-    <style>
-        .badge {
-            font-size: 75% !important;
-        }
-    </style>
-@endsection
-
 @section('content')
     <div class="mt-3 pb-3">
         <div class="mt-1 mb-3">
@@ -33,89 +25,33 @@
                     <hr/>
 
                     <h1>抽獎活動</h1>
-                    @if(isset($student->ticket))
-                        <div class="alert alert-success text-center">
-                            <i class="fas fa-ticket-alt"></i> 抽獎編號 <i class="fas fa-ticket-alt"></i>
-                            <h3>{{ sprintf("%04d", $student->ticket->id) }}</h3>
-                            <span class="text-danger">任務已完成，可至服務台兌換遊戲區代幣</span>
-                        </div>
-                    @else
-                        @if(!$student->is_freshman)
-                            <div class="alert alert-warning">
-                                <i class="fas fa-exclamation-triangle"></i> 抽獎活動限<strong>大學部新生</strong>參加，即使完成任務，也無法參加抽獎
-                            </div>
+                    @include('components.mission-info', ['student' => $student])
+                    <div class="text-center">
+                        @if($student->studentSurvey)
+                            <a href="{{ route('survey.student.show') }}" class="btn btn-success">
+                                <i class="fa fa-search"></i> 檢視平台問卷
+                            </a>
+                        @elseif(Carbon\Carbon::now()->gt(new Carbon\Carbon(Setting::get('end_at'))))
+                            <button type="button" class="btn btn-primary disabled">
+                                <i class="fa fa-edit"></i> 填寫平台問卷
+                            </button>
+                            <small class="text-danger">已超過填寫時間</small>
+                        @elseif(!$student->has_enough_counted_records)
+                            <button type="button" class="btn btn-primary disabled">
+                                <i class="fa fa-edit"></i> 填寫平台問卷
+                            </button><br/>
+                            <small class="text-danger">請先完成<strong>打卡集點</strong></small>
                         @else
-                            <div class="alert alert-info">
-                                <i class="fas fa-exclamation-triangle"></i> 完成以下任務，即可取得抽獎編號
-                            </div>
+                            <a href="{{ route('survey.student.edit') }}" class="btn btn-primary">
+                                <i class="fa fa-edit"></i> 填寫平台問卷
+                            </a>
                         @endif
-                    @endif
-                    <dl class="row" style="font-size: 120%">
-                        <dt class="col-md-3 col-lg-2">打卡集點</dt>
-                        <dd class="col-md-7 col-lg-10">
-                            @if($student->has_enough_counted_records)
-                                <span class="text-success">
-                                    <i class="far fa-check-square"></i> 已完成
-                                </span>
-                            @else
-                                <span class="text-danger">
-                                    <i class="far fa-square"></i> 未完成
-                                </span>
-                                <span>（{{ $student->countedRecords->count() }} / {{ \Setting::get('target') }}）</span>
-                            @endif
-                        </dd>
-                        <dt class="col-md-3 col-lg-2">填寫平台問卷</dt>
-                        <dd class="col-md-7 col-lg-10">
-                            <div class="mb-2">
-                                @if($student->studentSurvey)
-                                    <span class="text-success">
-                                        <i class="far fa-check-square"></i> 已完成
-                                    </span>
-                                @else
-                                    <span class="text-danger">
-                                        <i class="far fa-square"></i> 未完成
-                                    </span>
-                                @endif
-                            </div>
-                            @if($student->studentSurvey)
-                                <a href="{{ route('survey.student.show') }}" class="btn btn-success">
-                                    <i class="fa fa-search"></i> 檢視平台問卷
-                                </a>
-                            @elseif(Carbon\Carbon::now()->gt(new Carbon\Carbon(Setting::get('end_at'))))
-                                <button type="button" class="btn btn-primary disabled">
-                                    <i class="fa fa-edit"></i> 填寫平台問卷
-                                </button>
-                                <small class="text-danger">已超過填寫時間</small>
-                            @elseif(!$student->has_enough_counted_records)
-                                <button type="button" class="btn btn-primary disabled">
-                                    <i class="fa fa-edit"></i> 填寫平台問卷
-                                </button>
-                                <small class="text-danger">請先完成<strong>打卡集點</strong></small>
-                            @else
-                                <a href="{{ route('survey.student.edit') }}" class="btn btn-primary">
-                                    <i class="fa fa-edit"></i> 填寫平台問卷
-                                </a>
-                            @endif
-                        </dd>
-                    </dl>
+                    </div>
 
                     <hr/>
 
                     <h1>打卡集點</h1>
-                    @php
-                        $progress = ($student->countedRecords->count() / \Setting::get('target')) * 100;
-                        $progress = round($progress, 2);
-                    @endphp
-                    @include('components.progress-bar', compact('progress'))
-
-                    <dl class="row" style="font-size: 120%">
-                        <dt class="col-md-2">打卡次數</dt>
-                        <dd class="col-md-10">{{ $student->records->count() }}</dd>
-
-                        <dt class="col-md-2">進度</dt>
-                        <dd class="col-md-10">{{ $student->countedRecords->count() }}
-                            / {{ \Setting::get('target') }}</dd>
-                    </dl>
+                    @include('components.check-in-progress', ['student' => $student])
 
                     <hr/>
 
@@ -138,7 +74,8 @@
     </div>
 
     {{-- Alert Modal --}}
-    <div class="modal fade" id="alertModal" tabindex="-1" role="dialog" aria-labelledby="alertModalLabel" aria-hidden="true">
+    <div class="modal fade" id="alertModal" tabindex="-1" role="dialog" aria-labelledby="alertModalLabel"
+         aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -158,7 +95,8 @@
     </div>
 
     <!-- Confirm Modal -->
-    <div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="confirmModalLabel" aria-hidden="true">
+    <div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="confirmModalLabel"
+         aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
