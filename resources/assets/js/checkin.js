614c1dfd7c206d6c1dcf8f89e450f5f887f35bd8
Chan, Danny <danny50610@yahoo.com.tw>
2018-08-31 15:28:07 +0800

    使用 BS Model 顯示打卡成功訊息

--- resources/assets/js/checkin.js
+++ resources/assets/js/checkin.js
@@ -10,11 +10,41 @@ window.Echo.private('student.' + window.Laravel.student)
         }
 
         if (data.ask_for_feedback) {
-            let redirectToFeedback = confirm("於「" + data.club_name + "」打卡成功\n是否願意留下回饋資料？");
-            if (redirectToFeedback) {
+            confirmMessage = `於「${data.club_name}」打卡成功\n是否願意留下回饋資料？`;
+            confirmCallback = function () {
                 window.location.href = data.feedback_url;
-            }
+            };
+            $confirmModal.modal('show');
+
             return;
         }
-        alert("於「" + data.club_name + "」打卡成功");
+
+        // alert("於「" + data.club_name + "」打卡成功");
+        alertMessage = `於「${data.club_name}」打卡成功`;
+        $alertModal.modal('show');
     });
+
+let $alertModal, $confirmModal;
+let alertMessage = '';
+let confirmMessage = '';
+let confirmCallback = undefined;
+
+$(function () {
+    $alertModal = $('#alertModal');
+    $confirmModal = $('#confirmModal');
+
+    $alertModal.on('show.bs.modal', function (event) {
+        let modal = $(this);
+        modal.find('#alertMessage').text(alertMessage);
+    });
+
+    $confirmModal.on('show.bs.modal', function (event) {
+        let modal = $(this);
+        modal.find('#confirmMessage').html(confirmMessage.replace("\n", '<br/>'));
+        modal.find('#confirmButton').on('click', function () {
+            if (confirmCallback !== undefined) {
+                confirmCallback();
+            }
+        })
+    });
+});
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -135,6 +135,47 @@
             </div>
         </div>
     </div>
+
+    {{-- Alert Modal --}}
+    <div class="modal fade" id="alertModal" tabindex="-1" role="dialog" aria-labelledby="alertModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="alertModalLabel">訊息</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <span id="alertMessage"></span>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">確認</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Confirm Modal -->
+    <div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="confirmModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="confirmModalLabel">訊息</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <span id="confirmMessage"></span>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">稍後再說</button>
+                    <button type="button" class="btn btn-primary" id="confirmButton">前往填寫</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('js')
