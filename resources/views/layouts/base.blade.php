1a56fb66e0eb19f52aba3b61caa3b2fd0eb0e3bf
KID <jyhsu2000@yahoo.com.tw>
2019-04-30 00:26:31 +0800

    Move page title up

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
     <div class="mt-3 pb-3">
+        <h1>@yield('title')</h1>
         <div>
             @yield('buttons')
         </div>
-        <h1>@yield('title')</h1>
         <div class="mt-1 mb-3">
             @yield('main_content')
         </div>
