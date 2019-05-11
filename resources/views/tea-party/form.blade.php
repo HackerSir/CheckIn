9ba4b0dcb62fdc8e0cd6f5fbff03007a132134de
KID <jyhsu2000@yahoo.com.tw>
2019-05-11 12:19:44 +0800

    Update TeaParty edit page

--- resources/views/tea-party/form.blade.php
+++ resources/views/tea-party/form.blade.php
@@ -1,11 +1,19 @@
-{{ bs()->formGroup(bs()->select('club_id', \App\Club::selectOptions(), !isset($teaParty) ? request('club_id') : null)->required())->class('required')->label('社團')->showAsRow() }}
+@if(!isset($teaParty) && request('club_id'))
+    {{ bs()->formGroup(bs()->select('club_id', \App\Club::selectOptions(), request('club_id'))->disabled())->label('社團')->showAsRow() }}
+    {{ bs()->hidden('club_id', request('club_id')) }}
+@else
+    {{ bs()->formGroup(bs()->select('club_id', \App\Club::selectOptions())->required())->class('required')->label('社團')->showAsRow() }}
+@endif
+
 @include('tea-party.common-form')
 
 @section('js')
     @parent
-    <script>
-        $(function () {
-            $('select[name=club_id]').select2();
-        });
-    </script>
+    @unless(!isset($teaParty) && request('club_id'))
+        <script>
+            $(function () {
+                $('select[name=club_id]').select2();
+            });
+        </script>
+    @endunless
 @endsection
