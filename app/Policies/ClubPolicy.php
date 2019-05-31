addb65d89a445f4e502e3d367653b3ceb6eb1b75
KID <jyhsu2000@yahoo.com.tw>
2019-05-31 16:15:31 +0800

    Create ClubPolicy

---
+++ app/Policies/ClubPolicy.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Policies;
+
+use App\Club;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ClubPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * @param User $user
+     * @param string $ability
+     * @return bool
+     */
+    public function before(User $user, $ability)
+    {
+        //未登入，禁止所有功能
+        if (!$user) {
+            return false;
+        }
+        //若有權限，直接允許使用所有功能
+        if ($user->can('club.manage')) {
+            return true;
+        }
+
+        return null;
+    }
+
+    public function asStaff(User $user, Club $club)
+    {
+        if (!$user->student) {
+            return false;
+        }
+
+        return $club->students->contains($user->student);
+    }
+
+    public function asLeader(User $user, Club $club)
+    {
+        if (!$user->student) {
+            return false;
+        }
+        return $club->leaders->contains($user->student);
+    }
+}
