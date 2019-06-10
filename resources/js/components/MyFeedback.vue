f321d0a54078af5f3e0ffe13a68c949eaf5c0426
KID <jyhsu2000@yahoo.com.tw>
2019-06-10 18:49:23 +0800

    Fix typo

--- resources/js/components/MyFeedback.vue
+++ resources/js/components/MyFeedback.vue
@@ -30,7 +30,7 @@
 
                 <dt class="col-md-2">LINE ID</dt>
                 <dd class="col-md-10">
-                    <p v-if="feedback.email">{{ feedback.line }}</p>
+                    <p v-if="feedback.line">{{ feedback.line }}</p>
                     <p class="text-muted" v-else>（未填寫）</p>
                 </dd>
 
