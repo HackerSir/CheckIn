f67ac390e15c470eafe451d64622445e1b35ff52
KID <jyhsu2000@yahoo.com.tw>
2019-06-22 15:02:42 +0800

    Use conditional validation on contact info only if user has intention

--- app/Http/Requests/FeedbackRequest.php
+++ app/Http/Requests/FeedbackRequest.php
@@ -23,15 +23,24 @@ class FeedbackRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'include_phone'             => 'nullable|required_without_all:include_email,include_facebook,include_line',
-            'include_email'             => 'nullable|required_without_all:include_phone,include_facebook,include_line',
-            'include_facebook'          => 'nullable|required_without_all:include_phone,include_email,include_line',
-            'include_line'              => 'nullable|required_without_all:include_phone,include_email,include_facebook',
+        $rules = [
             'message'                   => 'nullable|max:255',
             'answer_of_custom_question' => 'nullable|max:255',
             'join_club_intention'       => 'required|in:0,1,2',
             'join_tea_party_intention'  => 'required|in:0,1,2',
         ];
+
+        //若非茶會與社團皆不參加，則必須勾選聯絡資訊
+        if ($this->request->get('join_club_intention') != 0
+            || $this->request->get('join_tea_party_intention') != 0) {
+            $rules = array_merge($rules, [
+                'include_phone'    => 'nullable|required_without_all:include_email,include_facebook,include_line',
+                'include_email'    => 'nullable|required_without_all:include_phone,include_facebook,include_line',
+                'include_facebook' => 'nullable|required_without_all:include_phone,include_email,include_line',
+                'include_line'     => 'nullable|required_without_all:include_phone,include_email,include_facebook',
+            ]);
+        }
+
+        return $rules;
     }
 }
