f2a1602c2e2655030e0c11037807cd7fba645cec
KID <jyhsu2000@yahoo.com.tw>
2019-06-25 11:43:59 +0800

    Show intention columns in FeedbackDatatables

--- resources/views/feedback/datatables/contact_info.blade.php
+++ resources/views/feedback/datatables/contact_info.blade.php
@@ -1,14 +1,14 @@
 <ul class="list-unstyled">
     @if($feedback->phone)
-        <li class="text-nowrap"><i class="fas fa-phone fa-fw mr-2" title="電話"></i>{{ $feedback->phone }}</li>
+        <li class="text-nowrap"><i class="fas fa-phone fa-fw mr-1" title="電話"></i>{{ $feedback->phone }}</li>
     @endif
     @if($feedback->email)
-        <li class="text-nowrap"><i class="fas fa-envelope fa-fw mr-2" title="信箱"></i>{{ $feedback->email }}</li>
+        <li class="text-nowrap"><i class="fas fa-envelope fa-fw mr-1" title="信箱"></i>{{ $feedback->email }}</li>
     @endif
     @if($feedback->facebook)
-        <li class="text-nowrap"><i class="fab fa-facebook-square fa-fw mr-2" title="Facebook"></i>{{ $feedback->facebook }}</li>
+        <li class="text-nowrap"><i class="fab fa-facebook-square fa-fw mr-1" title="Facebook"></i>{{ $feedback->facebook }}</li>
     @endif
     @if($feedback->line)
-        <li class="text-nowrap"><i class="fab fa-line fa-fw mr-2" title="LINE"></i>{{ $feedback->line }}</li>
+        <li class="text-nowrap"><i class="fab fa-line fa-fw mr-1" title="LINE"></i>{{ $feedback->line }}</li>
     @endif
 </ul>
