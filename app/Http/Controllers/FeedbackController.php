9e27fc8e48b937c6eec5a72bca5789922572a82d
KID <jyhsu2000@yahoo.com.tw>
2019-06-26 11:43:30 +0800

    Create FeedbackPolicy for authorizing viewing Feedback

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -161,33 +161,8 @@ class FeedbackController extends Controller
      */
     public function show(Feedback $feedback)
     {
-        //根據權限與身分檢查是否能看到
-        if (!\Laratrust::can('feedback.manage')) {
-            /** @var User $user */
-            $user = auth()->user();
-            if (\Gate::denies('as-staff', $feedback->club)
-                && (!$user->student || $user->student->nid != $feedback->student_nid)) {
-                abort(403);
-            }
-            //檢查檢視與下載期限
-            $feedbackDownloadExpiredAt = new Carbon(Setting::get('feedback_download_expired_at'));
-            if (Carbon::now()->gt($feedbackDownloadExpiredAt)) {
-                return back()->with('warning', '已超過檢視期限，若需查看資料，請聯繫各委會輔導老師');
-            }
-            $endAt = new Carbon(Setting::get('end_at'));
-            if (Carbon::now()->gt($endAt)) {
-                //活動結束
-                if (!$user->student) {
-                    //若無學生，直接返回
-                    return back()->with('warning', '已超過檢視期限，若需查看資料，請聯繫各委會輔導老師');
-                }
-                //確認是否為社長或資料擁有者
-                if (!($user->club && $user->club->pivot->is_leader)
-                    && ($user->student->nid != $feedback->student_nid)) {
-                    return redirect()->route('feedback.my')->with('warning', '活動已結束，僅社長可查看資料');
-                }
-            }
-        }
+        $this->authorize('view', $feedback);
+
         //打卡紀錄
         $record = Record::whereClubId($feedback->club_id)
             ->whereStudentNid($feedback->student_nid)
