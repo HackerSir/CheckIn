953062a4152d8f67b0f4973c7bcafc7f6252a1dd
KID <jyhsu2000@yahoo.com.tw>
2019-08-07 14:06:28 +0800

    Add hint

--- resources/views/payment-record/index.blade.php
+++ resources/views/payment-record/index.blade.php
@@ -17,6 +17,7 @@
 @section('main_content')
     <div class="alert alert-warning mb-1">
         <ul class="list-unstyled mb-0">
+            <li>此介面提供給社團整理學生繳費紀錄，方便管理社博現場收費狀況</li>
             <li>若<code>對應學生</code>欄位空白，表示該 NID 無效、該學生未填寫回饋資料，或該學生於回饋資料中表示對於參與社團與茶會皆無意願</li>
         </ul>
     </div>
