9837a9e89f4b083ccde55fdc3e2bf7d43165fe2c
KID <jyhsu2000@yahoo.com.tw>
2019-08-16 17:00:55 +0800

    Create LoggableModel

---
+++ app/LoggableModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class LoggableModel extends Model
+{
+    use LogsActivity;
+    protected static $logName = 'loggable-model';
+    protected static $logFillable = true;
+    protected static $logOnlyDirty = true;
+    protected static $eventText = [
+        'created' => '建立了',
+        'updated' => '更新了',
+        'deleted' => '刪除了',
+    ];
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return self::$eventText[$eventName] . ' ' . $this->name;
+    }
+}
