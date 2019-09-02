e9841b74349131ad1dfbb9bb67dcca82c16be737
jyhsu <jyhsu2000@gmail.com>
2019-09-02 20:50:13 +0800

    Add jump buttons to tea party list

--- resources/views/tea-party/list.blade.php
+++ resources/views/tea-party/list.blade.php
@@ -4,8 +4,16 @@
 
 
 @section('main_content')
-    <div class="card">
-        <div class="card-header">近期活動</div>
+    @if($inProcessTeaPartyCount = count($teaParties['in_process'] ?? [])>0)
+        <div class="alert alert-info">
+            目前有 {{ $inProcessTeaPartyCount }} 場活動正在進行中
+            <a href="javascript:void(0)" id="jumpToInProcessTeaParty"><strong>[按此查看]</strong></a>
+        </div>
+    @endif
+    <div class="card" id="notStartedTeaParty">
+        <div class="card-header d-flex">
+            近期活動
+        </div>
         <div class="card-body">
             <div class="container py-2">
                 @forelse($teaParties['not_started'] ?? [] as $teaParty)
@@ -16,8 +24,11 @@
             </div>
         </div>
     </div>
-    <div class="card mt-2">
-        <div class="card-header">進行中</div>
+    <div class="card mt-2" id="inProcessTeaParty">
+        <div class="card-header d-flex">
+            進行中
+            <a href="javascript:void(0)" class="ml-auto jumpToTop" title="回到最上方"><i
+                    class="fas fa-arrow-alt-circle-up mr-1"></i>TOP</a></div>
         <div class="card-body">
             <div class="container py-2">
                 @forelse($teaParties['in_process'] ?? [] as $teaParty)
@@ -29,8 +40,12 @@
         </div>
     </div>
     @if(count($teaParties['ended'] ?? []))
-        <div class="card mt-2">
-            <div class="card-header">已結束</div>
+        <div class="card mt-2" id="endedTeaParty">
+            <div class="card-header d-flex">
+                已結束
+                <a href="javascript:void(0)" class="ml-auto jumpToTop" title="回到最上方"><i
+                        class="fas fa-arrow-alt-circle-up mr-1"></i>TOP</a>
+            </div>
             <div class="card-body">
                 <div class="container py-2">
                     @foreach($teaParties['ended'] ?? [] as $teaParty)
@@ -40,4 +55,19 @@
             </div>
         </div>
     @endif
+@endsection
+
+@section('js')
+    <script>
+        $('#jumpToInProcessTeaParty').on('click', function () {
+            document.getElementById('inProcessTeaParty').scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+        $('.jumpToTop').on('click', function () {
+            document.getElementsByTagName('body')[0].scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    </script>
 @endsection
