3a6b291f161e846f489908d712c6b678ec14c806
KID <jyhsu2000@yahoo.com.tw>
2020-08-19 21:51:49 +0800

    Add Instagram links in footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,8 @@
     <div class="container" style="padding: 15px 10px">
         <p class="text-muted" style="margin-bottom: 8px">
             ©2017-{{ \Carbon\Carbon::now()->year }}
-            <a href="https://www.facebook.com/HackerSir.tw" target="_blank" style="color: #636c72;">逢甲大學黑客社 HackerSir</a>.
+            <a href="https://www.facebook.com/HackerSir.tw" target="_blank" style="color: #636c72;">逢甲大學黑客社
+                HackerSir</a>.
             All rights reserved.
         </p>
         <ol class="breadcrumb" style="padding: 0; margin-bottom: 0; background-color: #292b2c;">
@@ -20,6 +21,14 @@
                 {!! link_to_route('development-team', '開發團隊', [], [
                         'style' => 'color: #636c72;',
                     ]) !!}
+            </li>
+            <li class="breadcrumb-item">
+                <a href="https://www.instagram.com/fcu_hackersir/" target="_blank" style="color: #636c72;"><i
+                        class="fab fa-instagram mr-1"></i>黑客社</a>
+            </li>
+            <li class="breadcrumb-item">
+                <a href="https://www.instagram.com/fcu_afterclass/" target="_blank" style="color: #636c72;"><i
+                        class="fab fa-instagram mr-1"></i>課外活動組</a>
             </li>
             @if(Auth::guest())
                 <li class="breadcrumb-item">
