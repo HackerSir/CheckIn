ca4dbabdc9f08a945575413ffb7d2ba5f0b568aa
KID <jyhsu2000@yahoo.com.tw>
2020-08-19 22:25:40 +0800

    Add filter of TeaParty

--- resources/views/tea-party/list.blade.php
+++ resources/views/tea-party/list.blade.php
@@ -3,6 +3,34 @@
 @section('title', '迎新茶會')
 
 @section('main_content')
+    <div class="d-flex">
+        <div class="btn-group my-1" role="group">
+            <a href="{{ rtrim(request()->fullUrlWithQuery(['type' => null]), '?') }}"
+               class="btn btn-secondary @if (!request('type')) active @endif">全部</a>
+            <a href="{{ request()->fullUrlWithQuery(['type' => 'favorite']) }}"
+               class="btn btn-secondary @if (request('type') == 'favorite') active @endif">收藏社團</a>
+            <a href="{{ request()->fullUrlWithQuery(['type' => 'join']) }}"
+               class="btn btn-secondary @if (request('type') == 'join') active @endif">考慮或已登記參加茶會</a>
+        </div>
+        <div class="ml-auto">
+            <form action="{{ url()->current() }}" method="get" class="form-inline">
+                <div class="input-group is-invalid">
+                    @if(request('type'))
+                        <input type="hidden" name="type" value="{{ request('type') }}">
+                    @endif
+                    <input type="text" class="form-control" name="q" value="{{ request('q') }}" placeholder="搜尋"
+                           required>
+                    <div class="input-group-append">
+                        @if(request('q'))
+                            <a href="{{ rtrim(request()->fullUrlWithQuery(['q' => null]), '?') }}"
+                               class="btn btn-secondary"><i class="fas fa-eraser"></i></a>
+                        @endif
+                        <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i></button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
     @if($inProcessTeaPartyCount = count($teaParties['in_process'] ?? [])>0)
         <div class="alert alert-info">
             目前有 {{ $inProcessTeaPartyCount }} 場活動正在進行中
