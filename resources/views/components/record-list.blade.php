8ab30fef0af7784cfe8e22181959c7e2f93b4825
KID <jyhsu2000@yahoo.com.tw>
2020-08-21 00:21:59 +0800

    Show feedback in student show page

--- resources/views/components/record-list.blade.php
+++ resources/views/components/record-list.blade.php
@@ -1,17 +1,35 @@
 <ul class="list-group">
     @forelse($student->records as $record)
-        <li class="list-group-item clearfix">
-            <h4 class="mb-1">{{ link_to_route('clubs.show', $record->club->name, $record->club) }}
-                {!! $record->club->clubType->tag ?? '' !!}
-                @if(!$record->club->is_counted)
-                    <span class='badge badge-secondary'>不列入集點</span>
+        <li class="list-group-item d-flex flex-column flex-md-row">
+            <div>
+                <h4 class="mb-1">{{ link_to_route('clubs.show', $record->club->name, $record->club) }}
+                    {!! $record->club->clubType->tag ?? '' !!}
+                    @if(!$record->club->is_counted)
+                        <span class='badge badge-secondary'>不列入集點</span>
+                    @endif
+                </h4>
+                <div class="text-muted d-flex flex-wrap">
+                    <div title="{{ $record->created_at }}" class="d-inline-block mr-2">
+                        <i class="fas fa-clock mr-1"></i>{{ (new \Carbon\Carbon($record->created_at))->diffForHumans() }}
+                    </div>
+                    @if($booth = $record->club->booths->first())
+                        <div class="d-inline-block">
+                            <i class="fas fa-store"></i>
+                            @if($booth->zone)
+                                <span class="badge badge-secondary">{{ $booth->zone }}</span>
+                            @endif
+                            {{ $booth->name }}
+                        </div>
+                    @endif
+                </div>
+            </div>
+            <div class="ml-md-auto align-self-center">
+                @if($feedbackItem = $feedback->get($record->club->id))
+                    <a href="{{ route('feedback.show', $feedbackItem) }}" class="btn btn-success">
+                        <i class="fa fa-search"></i> 檢視回饋資料
+                    </a>
                 @endif
-            </h4>
-            <p>
-                <span title="{{ $record->created_at }}">
-                    {{ (new \Carbon\Carbon($record->created_at))->diffForHumans() }}
-                </span>
-            </p>
+            </div>
         </li>
     @empty
         <li class="list-group-item">
