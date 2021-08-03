52d819577b8a0258b323293d98d519d5c0065bc5
KID <jyhsu2000@gmail.com>
2021-08-03 20:04:13 +0800

    Upgrade `vue-qrcode-reader` to support `Scan Same QR Code More Than Once`

--- resources/js/components/WebScan.vue
+++ resources/js/components/WebScan.vue
@@ -7,7 +7,7 @@
             </ul>
         </div>
         <p>{{ message }}</p>
-        <qrcode-stream @decode="onDecode" @init="onInit"></qrcode-stream>
+        <qrcode-stream :camera="camera" :track="paintOutline" @decode="onDecode" @init="onInit"></qrcode-stream>
         <!-- Confirm Modal -->
         <transition @enter="startTransitionModal" @after-enter="endTransitionModal"
                     @before-leave="endTransitionModal" @after-leave="startTransitionModal">
@@ -42,6 +42,7 @@ export default {
     name: "WebScan",
     data() {
         return {
+            camera: 'auto',
             code: 'XXXXXXXX',
             message: '啟動中...',
             showModal: false,
@@ -92,7 +93,7 @@ export default {
                 // hide loading indicator
             }
         },
-        onDecode(decodedString) {
+        async onDecode(decodedString) {
             this.message = '掃描結果： ' + decodedString;
             // 檢查是否為條碼
             let scanPattern = new UrlPattern(/\/qr\/(.*?)(?:\?.*)?$/, ['code']);
@@ -104,6 +105,39 @@ export default {
             this.code = scanMatch.code;
             // 顯示提示視窗
             this.showModal = true;
+
+            // 重置 camera，使其能連續多次讀取同一組 QRCode
+            // https://gruhn.github.io/vue-qrcode-reader/demos/ScanSameQrcodeMoreThanOnce.html
+            this.pause()
+            await this.timeout(500)
+            this.unpause()
+        },
+        unpause() {
+            this.camera = 'auto'
+        },
+        pause() {
+            this.camera = 'off'
+        },
+        timeout(ms) {
+            return new Promise(resolve => {
+                window.setTimeout(resolve, ms)
+            })
+        },
+        paintOutline(detectedCodes, ctx) {
+            for (const detectedCode of detectedCodes) {
+                const [firstPoint, ...otherPoints] = detectedCode.cornerPoints
+
+                ctx.strokeStyle = "red";
+
+                ctx.beginPath();
+                ctx.moveTo(firstPoint.x, firstPoint.y);
+                for (const {x, y} of otherPoints) {
+                    ctx.lineTo(x, y);
+                }
+                ctx.lineTo(firstPoint.x, firstPoint.y);
+                ctx.closePath();
+                ctx.stroke();
+            }
         },
         startTransitionModal() {
             this.$refs.backdrop.classList.toggle("d-block");
