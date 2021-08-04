64ab9f16d3cd5872c50029ffefbabdbc65496b4c
KID <jyhsu2000@gmail.com>
2021-08-04 12:12:35 +0800

    Add icons in navbar on mobile

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,6 +7,16 @@
         <a class="navbar-brand font-weight-bold" style="margin-left: 1rem" href="{{ url('/') }}">
             {{ config('app.cht_name') }}
         </a>
+        <ul class="navbar-nav d-flex flex-row d-xl-none ml-auto">
+            <li class="nav-item" style="font-size: 20px">
+                <a href="{{ route('my-qrcode') }}" class="nav-link"><i class="fas fa-qrcode mx-2"></i></a>
+            </li>
+            @if(auth()->user()->club)
+                <li class="nav-item" style="font-size: 20px">
+                    <a href="{{ route('qrcode.web-scan') }}" class="nav-link"><i class="fas fa-camera mx-2"></i></a>
+                </li>
+            @endif
+        </ul>
 
         {{-- Item 會由 LaravelMenu 生成 --}}
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
