90f7df1539a4f22c78a0980517119d42675a518d
KID <jyhsu2000@gmail.com>
2021-10-05 12:31:44 +0800

    Update static maps

--- resources/views/map/static.blade.php
+++ resources/views/map/static.blade.php
@@ -4,13 +4,13 @@
 
 @section('main_content')
     <div class="mt-2">
-        <a href="https://i.imgur.com/BUNz8V0.jpg" target="_blank">
-            <img src="https://i.imgur.com/BUNz8V0.jpg" class="img-fluid w-100" alt="static map">
+        <a href="{{ asset('img/static/static_map_1.jpg') }}" target="_blank">
+            <img src="{{ asset('img/static/static_map_1.jpg') }}" class="img-fluid w-100" alt="static map">
         </a>
     </div>
     <div class="mt-2">
-        <a href="https://i.imgur.com/GhQrAkN.jpg" target="_blank">
-            <img src="https://i.imgur.com/GhQrAkN.jpg" class="img-fluid w-100" alt="static map">
+        <a href="{{ asset('img/static/static_map_2.jpg') }}" target="_blank">
+            <img src="{{ asset('img/static/static_map_2.jpg') }}" class="img-fluid w-100" alt="static map">
         </a>
     </div>
 @endsection
