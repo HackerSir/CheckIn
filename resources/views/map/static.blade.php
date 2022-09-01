6869d5ce52e9838f43f151457f1bbab9725f85d1
jyhsu <jyhsu2000@gmail.com>
2022-09-01 17:30:21 +0800

    Update static map

--- resources/views/map/static.blade.php
+++ resources/views/map/static.blade.php
@@ -3,14 +3,9 @@
 @section('title', '靜態地圖 - 攤位地圖')
 
 @section('main_content')
-    {{--    <div class="mt-2">--}}
-    {{--        <a href="{{ asset('img/static/static_map_1.jpg') }}" target="_blank">--}}
-    {{--            <img src="{{ asset('img/static/static_map_1.jpg') }}" class="img-fluid w-100" alt="static map">--}}
-    {{--        </a>--}}
-    {{--    </div>--}}
-    {{--    <div class="mt-2">--}}
-    {{--        <a href="{{ asset('img/static/static_map_2.jpg') }}" target="_blank">--}}
-    {{--            <img src="{{ asset('img/static/static_map_2.jpg') }}" class="img-fluid w-100" alt="static map">--}}
-    {{--        </a>--}}
-    {{--    </div>--}}
+    <div class="mt-2">
+        <a href="{{ asset('img/static/static_map_1.png') }}" target="_blank">
+            <img src="{{ asset('img/static/static_map_1.png') }}" class="img-fluid w-100" alt="static map">
+        </a>
+    </div>
 @endsection
